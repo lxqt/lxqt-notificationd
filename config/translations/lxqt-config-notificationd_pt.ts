edf887014a1312aec61a345c298da447c4970d33
Sérgio Marques <smarquespt@gmail.com>
2015-01-06 18:31:06 +0000

    Portuguese language update

---
+++ config/translations/lxqt-config-notificationd_pt.ts
@@ -0,0 +1,109 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt">
+<context>
+    <name>AdvancedSettings</name>
+    <message>
+        <source>Menu Configuration</source>
+        <translation>Configuração do menu</translation>
+    </message>
+    <message>
+        <source>Notification Duration</source>
+        <translation>Duração da notificação</translation>
+    </message>
+    <message>
+        <source>For some the notification&apos;s expiration time is dependent on the notification server&apos;s settings.</source>
+        <translation>Para algumas notificações, o tempo limite depende das definições das notificações do servidor.</translation>
+    </message>
+    <message>
+        <source>&quot;Server Decides&quot; Time:</source>
+        <translation>Tempo para &quot;Decisão do servidor&quot;:</translation>
+    </message>
+    <message>
+        <source> sec</source>
+        <translation> seg</translation>
+    </message>
+    <message>
+        <source>Sizes</source>
+        <translation>Tamanhos</translation>
+    </message>
+    <message>
+        <source>Notification Spacing:</source>
+        <translation>Espaçamento da notificação:</translation>
+    </message>
+    <message>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <source>Notification Width:</source>
+        <translation>Largura da notificaçao:</translation>
+    </message>
+</context>
+<context>
+    <name>BasicSettings</name>
+    <message>
+        <source>Basic Settings</source>
+        <translation>Definições básicas</translation>
+    </message>
+    <message>
+        <source>Display Notifications</source>
+        <translation>Mostrar notificações</translation>
+    </message>
+    <message>
+        <source>Top/Left</source>
+        <translation>Cima/Esquerda</translation>
+    </message>
+    <message>
+        <source>Top/Right</source>
+        <translation>Cima/Direita</translation>
+    </message>
+    <message>
+        <source>Bottom/Left</source>
+        <translation>Baixo/Esquerda</translation>
+    </message>
+    <message>
+        <source>Bottom/Right</source>
+        <translation>Baixo/Direita</translation>
+    </message>
+    <message>
+        <source>Test Notification</source>
+        <translation>Testar notificação</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
+A fallback will be used.</source>
+        <translation>&lt;b&gt;Aviso:&lt;/b&gt; o serviço de notificaçoes não está em execução.
+Será utilizada uma solução alternativa.</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
+These settings won&apos;t have an effect on it!</source>
+        <translation>&lt;b&gt;Aviso:&lt;/b&gt; já está a ser executado um serviço de notificações (%1).
+Estas definições não produzirão qualquer efeito!</translation>
+    </message>
+    <message>
+        <source>Notification Summary</source>
+        <translation>Resumo da notificação</translation>
+    </message>
+    <message>
+        <source>Notification Body</source>
+        <translation>Corpo da notificação</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Desktop Notifications</source>
+        <translation>Notificações do ambiente de trabalho</translation>
+    </message>
+    <message>
+        <source>Basic Settings</source>
+        <translation>Definições básicas</translation>
+    </message>
+    <message>
+        <source>Advanced Settings</source>
+        <translation>Definições avançadas</translation>
+    </message>
+</context>
+</TS>
