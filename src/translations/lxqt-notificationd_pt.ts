89a4d7ed8a5e1e8847d3d6e3f409063741d4fad4
Sérgio Marques <smarquespt@gmail.com>
2015-01-06 18:31:06 +0000

    Portuguese language update

---
+++ src/translations/lxqt-notificationd_pt.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt">
+<context>
+    <name>NotificationActionsComboWidget</name>
+    <message>
+        <source>Actions:</source>
+        <translation>Ações:</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+</TS>
