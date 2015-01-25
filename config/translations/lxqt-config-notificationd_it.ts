1aee3e7686ca0d0c02a9dbbd38e3d34e7e5dbea3
Stef Binde <stef.binde@riseup.net>
2015-01-25 21:40:54 +0100

    Create lxqt-config-notificationd_it.ts
    Source file seems not correct, some text still english and different (e.g Position on screen)

---
+++ config/translations/lxqt-config-notificationd_it.ts
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it">
+<context>
+    <name>AdvancedSettings</name>
+    <message>
+        <source>Menu Configuration</source>
+        <translation>Configurazione del menu</translation>
+    </message>
+    <message>
+        <source>Notification Duration</source>
+        <translation>Durata delle notifiche</translation>
+    </message>
+    <message>
+        <source>For some the notification&apos;s expiration time is dependent on the notification server&apos;s settings.</source>
+        <translation>Per alcune notifiche il tempo dipende dalle impostazioni del server delle notifiche.</translation>
+    </message>
+    <message>
+        <source>&quot;Server Decides&quot; Time:</source>
+        <translation>Tempo di decisioni del server:</translation>
+    </message>
+    <message>
+        <source> sec</source>
+        <translation> sec</translation>
+    </message>
+    <message>
+        <source>Sizes</source>
+        <translation>Dimensione</translation>
+    </message>
+    <message>
+        <source>Notification Spacing:</source>
+        <translatorcomment>a cosa si riferisce??</translatorcomment>
+        <translation type="unfinished">Spazing:</translation>
+    </message>
+    <message>
+        <source> px</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Notification Width:</source>
+        <translation>Larghezza:</translation>
+    </message>
+</context>
+<context>
+    <name>BasicSettings</name>
+    <message>
+        <source>Basic Settings</source>
+        <translation>Impostazioni base</translation>
+    </message>
+    <message>
+        <source>Display Notifications</source>
+        <translation>Mostra notifiche</translation>
+    </message>
+    <message>
+        <source>Top/Left</source>
+        <translation>Alto a sinistra</translation>
+    </message>
+    <message>
+        <source>Top/Right</source>
+        <translation>Alto a destra</translation>
+    </message>
+    <message>
+        <source>Bottom/Left</source>
+        <translation>Fondo a sinistra</translation>
+    </message>
+    <message>
+        <source>Bottom/Right</source>
+        <translation>Fondo a destra</translation>
+    </message>
+    <message>
+        <source>Test Notification</source>
+        <translation>Notifica di prova</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
+A fallback will be used.</source>
+        <translation>&lt;b&gt;Attenzione:&lt;/b&gt; Nessun demone di notifiche è in esecuzione.
+Sarà usato un sistema di riserva.</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
+These settings won&apos;t have an effect on it!</source>
+        <translation>&lt;b&gt;Attenzione:&lt;/b&gt; Un altro demone di notifiche (%1) è in esecuzione.
+Queste impostazioni non avranno effetto su quello!</translation>
+    </message>
+    <message>
+        <source>Notification Summary</source>
+        <translation>Sommario della notifica</translation>
+    </message>
+    <message>
+        <source>Notification Body</source>
+        <translation>Corpo della notifica</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Desktop Notifications</source>
+        <translation type="unfinished">Notificazioni Desktop</translation>
+    </message>
+    <message>
+        <source>Basic Settings</source>
+        <translation>Impostazioni base</translation>
+    </message>
+    <message>
+        <source>Advanced Settings</source>
+        <translation>Impostazioni avanzate</translation>
+    </message>
+</context>
+</TS>
