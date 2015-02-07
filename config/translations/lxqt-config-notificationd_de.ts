7a24dbf47b0669b507493e16f6fd4e25886e8cec
Thomas Viehweger <ThomasVie@users.noreply.github.com>
2015-02-07 22:39:48 +0100

    Added german translation.

---
+++ config/translations/lxqt-config-notificationd_de.ts
@@ -0,0 +1,109 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>AdvancedSettings</name>
+    <message>
+        <source>Menu Configuration</source>
+        <translation>Menü Konfiguration</translation>
+    </message>
+    <message>
+        <source>Notification Duration</source>
+        <translation>Benachrichtigungsdauer</translation>
+    </message>
+    <message>
+        <source>For some the notification&apos;s expiration time is dependent on the notification server&apos;s settings.</source>
+        <translation>Einige Anzeigedauern hängen von den Einstellungen des Benachrichtigungsservers ab.</translation>
+    </message>
+    <message>
+        <source>&quot;Server Decides&quot; Time:</source>
+        <translation>&quot;Server entscheidet&quot;-Zeit:</translation>
+    </message>
+    <message>
+        <source> sec</source>
+        <translation> s</translation>
+    </message>
+    <message>
+        <source>Sizes</source>
+        <translation>Größen</translation>
+    </message>
+    <message>
+        <source>Notification Spacing:</source>
+        <translation>Abstand der Benachrichtigungen:</translation>
+    </message>
+    <message>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <source>Notification Width:</source>
+        <translation>Breite der Benachrichtigungen:</translation>
+    </message>
+</context>
+<context>
+    <name>BasicSettings</name>
+    <message>
+        <source>Basic Settings</source>
+        <translation>Grundeinstellungen</translation>
+    </message>
+    <message>
+        <source>Display Notifications</source>
+        <translation>Benachrichtigungen anzeigen</translation>
+    </message>
+    <message>
+        <source>Top/Left</source>
+        <translation>Links oben</translation>
+    </message>
+    <message>
+        <source>Top/Right</source>
+        <translation>Rechts oben</translation>
+    </message>
+    <message>
+        <source>Bottom/Left</source>
+        <translation>Links unten</translation>
+    </message>
+    <message>
+        <source>Bottom/Right</source>
+        <translation>Rechts unten</translation>
+    </message>
+    <message>
+        <source>Test Notification</source>
+        <translation>Benachrichtigungstest</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
+A fallback will be used.</source>
+        <translation>&lt;b&gt;Warnung:&lt;/b&gt; Es läuft kein Benachrichtigungsdaemon.
+Ein Ersatz wird verwendet.</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
+These settings won&apos;t have an effect on it!</source>
+        <translation>&lt;b&gt;Warnung:&lt;/b&gt; Ein anderer Benachrichtigungsdaemon (%1) läuft.
+Diese Einstellungen wirken sich auf ihn nicht aus!</translation>
+    </message>
+    <message>
+        <source>Notification Summary</source>
+        <translation>Benachrichtigungszusammenfassung</translation>
+    </message>
+    <message>
+        <source>Notification Body</source>
+        <translation>Benachrichtigungsrumpf</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Desktop Notifications</source>
+        <translation>Arbeitsflächenbenachrichtigungen</translation>
+    </message>
+    <message>
+        <source>Basic Settings</source>
+        <translation>Grundeinstellungen</translation>
+    </message>
+    <message>
+        <source>Advanced Settings</source>
+        <translation>Erweiterte Einstellungen</translation>
+    </message>
+</context>
+</TS>
