6272b59df26e44b4d09b66a70e8c07d6d0f7105c
attuska <attus@enterpriseforever.com>
2015-05-27 14:28:36 +0200

    Hungarian translation added, modified

---
+++ src/translations/lxqt-notificationd_hu.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu">
+<context>
+    <name>NotificationActionsComboWidget</name>
+    <message>
+        <source>Actions:</source>
+        <translation>Akciók:</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
