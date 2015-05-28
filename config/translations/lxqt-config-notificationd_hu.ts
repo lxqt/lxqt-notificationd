600f01fd826dbeeee82dac4c21d26e6d9ed735c4
attuska <attus@enterpriseforever.com>
2015-05-28 13:59:35 +0200

    *_hu.ts added

---
+++ config/translations/lxqt-config-notificationd_hu.ts
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu">
+<context>
+    <name>AdvancedSettings</name>
+    <message>
+        <source>Menu Configuration</source>
+        <translatorcomment>Menübeállítás</translatorcomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Notification Duration</source>
+        <translation>Jelzési idő</translation>
+    </message>
+    <message>
+        <source>For some the notification&apos;s expiration time is dependent on the notification server&apos;s settings.</source>
+        <translation>Egyes jelzési idők az értesítő szerver beállításaitól függnek.</translation>
+    </message>
+    <message>
+        <source>&quot;Server Decides&quot; Time:</source>
+        <translation>Szervertől függő idő:</translation>
+    </message>
+    <message>
+        <source> sec</source>
+        <translation> mp</translation>
+    </message>
+    <message>
+        <source>Sizes</source>
+        <translation>Méret</translation>
+    </message>
+    <message>
+        <source>Notification Spacing:</source>
+        <translation>Értesítési köz:</translation>
+    </message>
+    <message>
+        <source> px</source>
+        <translation> pixel</translation>
+    </message>
+    <message>
+        <source>Notification Width:</source>
+        <translation>Értesítési szélesség:</translation>
+    </message>
+</context>
+<context>
+    <name>BasicSettings</name>
+    <message>
+        <source>Basic Settings</source>
+        <translation>Alapbeállítások</translation>
+    </message>
+    <message>
+        <source>Display Notifications</source>
+        <translation>Értesítések jelzése</translation>
+    </message>
+    <message>
+        <source>Top/Left</source>
+        <translation>Balra fenn</translation>
+    </message>
+    <message>
+        <source>Top/Right</source>
+        <translation>Jobbra fenn</translation>
+    </message>
+    <message>
+        <source>Bottom/Left</source>
+        <translation>Balra lenn</translation>
+    </message>
+    <message>
+        <source>Bottom/Right</source>
+        <translation>Jobbra lenn</translation>
+    </message>
+    <message>
+        <source>Test Notification</source>
+        <translation>Értesítési teszt</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
+A fallback will be used.</source>
+        <translation>&lt;b&gt;Figyelem:&lt;/b&gt; Értesítő démon nem fut.
+A vésztartalék használatos.</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
+These settings won&apos;t have an effect on it!</source>
+        <translation>&lt;b&gt;Figyelem:&lt;/b&gt; A (%1) démont már valaki futtatja.
+Ezek a beállítások arra már hatástalanok!</translation>
+    </message>
+    <message>
+        <source>Notification Summary</source>
+        <translation>Értesítési összegző</translation>
+    </message>
+    <message>
+        <source>Notification Body</source>
+        <translation>Értesítési törzs</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Desktop Notifications</source>
+        <translation>Asztali értesítések</translation>
+    </message>
+    <message>
+        <source>Basic Settings</source>
+        <translation>Alapbeállítások</translation>
+    </message>
+    <message>
+        <source>Advanced Settings</source>
+        <translation>Haladó beállítások</translation>
+    </message>
+</context>
+</TS>
