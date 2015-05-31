534afbb12a3e4d885275700832fd2b0766028090
attuska <attus@enterpriseforever.com>
2015-05-31 20:42:26 +0200

    config-nototification_hu.ts modified

--- config/translations/lxqt-config-notificationd_hu.ts
+++ config/translations/lxqt-config-notificationd_hu.ts
@@ -1,108 +1,123 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="hu">
+<TS version="2.1" language="hu_HU">
 <context>
     <name>AdvancedSettings</name>
     <message>
-        <source>Menu Configuration</source>
-        <translatorcomment>Menübeállítás</translatorcomment>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Notification Duration</source>
-        <translation>Jelzési idő</translation>
-    </message>
-    <message>
-        <source>For some the notification&apos;s expiration time is dependent on the notification server&apos;s settings.</source>
-        <translation>Egyes jelzési idők az értesítő szerver beállításaitól függnek.</translation>
-    </message>
-    <message>
-        <source>&quot;Server Decides&quot; Time:</source>
-        <translation>Szervertől függő idő:</translation>
-    </message>
-    <message>
-        <source> sec</source>
-        <translation> mp</translation>
-    </message>
-    <message>
+        <location filename="../advancedsettings.ui" line="17"/>
         <source>Sizes</source>
         <translation>Méret</translation>
     </message>
     <message>
-        <source>Notification Spacing:</source>
-        <translation>Értesítési köz:</translation>
+        <location filename="../advancedsettings.ui" line="25"/>
+        <source>Width:</source>
+        <translation>Szélesség:</translation>
     </message>
     <message>
+        <location filename="../advancedsettings.ui" line="32"/>
+        <location filename="../advancedsettings.ui" line="56"/>
         <source> px</source>
         <translation> pixel</translation>
     </message>
     <message>
-        <source>Notification Width:</source>
-        <translation>Értesítési szélesség:</translation>
+        <location filename="../advancedsettings.ui" line="49"/>
+        <source>Spacing:</source>
+        <translation>Térköz:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="68"/>
+        <source>Duration</source>
+        <translation>Tartam</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="74"/>
+        <source>Some notifications set their own on-screen duration.</source>
+        <translation>Egyes értesítések megjelenési ideje</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="86"/>
+        <source>Default duration:</source>
+        <translation>Alapértelmezett tartam:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="93"/>
+        <source> sec</source>
+        <translation> mp</translation>
     </message>
 </context>
 <context>
     <name>BasicSettings</name>
     <message>
+        <location filename="../basicsettings.ui" line="23"/>
         <source>Basic Settings</source>
         <translation>Alapbeállítások</translation>
     </message>
     <message>
-        <source>Display Notifications</source>
-        <translation>Értesítések jelzése</translation>
+        <location filename="../basicsettings.ui" line="30"/>
+        <source>Position on screen</source>
+        <translation>Hely</translation>
     </message>
     <message>
+        <location filename="../basicsettings.ui" line="36"/>
         <source>Top/Left</source>
         <translation>Balra fenn</translation>
     </message>
     <message>
+        <location filename="../basicsettings.ui" line="43"/>
         <source>Top/Right</source>
         <translation>Jobbra fenn</translation>
     </message>
     <message>
+        <location filename="../basicsettings.ui" line="50"/>
         <source>Bottom/Left</source>
         <translation>Balra lenn</translation>
     </message>
     <message>
+        <location filename="../basicsettings.ui" line="57"/>
         <source>Bottom/Right</source>
         <translation>Jobbra lenn</translation>
     </message>
     <message>
-        <source>Test Notification</source>
-        <translation>Értesítési teszt</translation>
-    </message>
-    <message>
+        <location filename="../basicsettings.cpp" line="53"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
 A fallback will be used.</source>
         <translation>&lt;b&gt;Figyelem:&lt;/b&gt; Értesítő démon nem fut.
 A vésztartalék használatos.</translation>
     </message>
     <message>
+        <location filename="../basicsettings.cpp" line="59"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
-These settings won&apos;t have an effect on it!</source>
+These settings won&apos;t have any effect on it!</source>
         <translation>&lt;b&gt;Figyelem:&lt;/b&gt; A (%1) démont már valaki futtatja.
 Ezek a beállítások arra már hatástalanok!</translation>
     </message>
     <message>
-        <source>Notification Summary</source>
-        <translation>Értesítési összegző</translation>
+        <location filename="../basicsettings.cpp" line="98"/>
+        <source>Notification demo</source>
+        <translation>Értesítési bemutató</translation>
     </message>
     <message>
-        <source>Notification Body</source>
-        <translation>Értesítési törzs</translation>
+        <location filename="../basicsettings.cpp" line="99"/>
+        <source>This is a test notification.
+All notifications will now appear here on LXQt.</source>
+        <translation>Az értesítés tesztje
+Minden értesítést megjelenít az LXQt.</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
+        <location filename="../mainwindow.cpp" line="40"/>
         <source>Desktop Notifications</source>
         <translation>Asztali értesítések</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="43"/>
         <source>Basic Settings</source>
         <translation>Alapbeállítások</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="47"/>
         <source>Advanced Settings</source>
         <translation>Haladó beállítások</translation>
     </message>
