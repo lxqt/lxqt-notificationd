fa6199c5b88ac31e75bd7b8038e0ea3409e87640
Thomas Viehweger <ThomasVie@users.noreply.github.com>
2015-06-27 15:44:09 +0200

    Updated german translation.

--- config/translations/lxqt-config-notificationd_de.ts
+++ config/translations/lxqt-config-notificationd_de.ts
@@ -4,104 +4,100 @@
 <context>
     <name>AdvancedSettings</name>
     <message>
-        <source>Menu Configuration</source>
-        <translation>Menü Konfiguration</translation>
-    </message>
-    <message>
-        <source>Notification Duration</source>
-        <translation>Benachrichtigungsdauer</translation>
-    </message>
-    <message>
-        <source>For some the notification&apos;s expiration time is dependent on the notification server&apos;s settings.</source>
-        <translation>Einige Anzeigedauern hängen von den Einstellungen des Benachrichtigungsservers ab.</translation>
-    </message>
-    <message>
-        <source>&quot;Server Decides&quot; Time:</source>
-        <translation>Vom Server bestimmte Zeit:</translation>
-    </message>
-    <message>
-        <source> sec</source>
-        <translation> s</translation>
-    </message>
-    <message>
+        <location filename="../advancedsettings.ui" line="17"/>
         <source>Sizes</source>
         <translation>Größen</translation>
     </message>
     <message>
-        <source>Notification Spacing:</source>
-        <translation>Abstand der Benachrichtigungen:</translation>
+        <location filename="../advancedsettings.ui" line="25"/>
+        <source>Width:</source>
+        <translation>Breite:</translation>
     </message>
     <message>
+        <location filename="../advancedsettings.ui" line="32"/>
+        <location filename="../advancedsettings.ui" line="56"/>
         <source> px</source>
         <translation> px</translation>
     </message>
     <message>
-        <source>Notification Width:</source>
-        <translation>Breite der Benachrichtigungen:</translation>
+        <location filename="../advancedsettings.ui" line="49"/>
+        <source>Spacing:</source>
+        <translation>Abstand:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="68"/>
+        <source>Duration</source>
+        <translation>Dauer</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="74"/>
+        <source>Some notifications set their own on-screen duration.</source>
+        <translation>Einige Benachrichtigungen setzen ihre eigene Anzeigedauer.</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="86"/>
+        <source>Default duration:</source>
+        <translation>Standarddauer:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="93"/>
+        <source> sec</source>
+        <translation> s</translation>
     </message>
 </context>
 <context>
     <name>BasicSettings</name>
     <message>
+        <location filename="../basicsettings.ui" line="23"/>
         <source>Basic Settings</source>
         <translation>Grundeinstellungen</translation>
     </message>
     <message>
-        <source>Display Notifications</source>
-        <translation>Benachrichtigungen anzeigen</translation>
+        <location filename="../basicsettings.ui" line="30"/>
+        <source>Position on screen</source>
+        <translation>Position auf dem Bildschirm</translation>
     </message>
     <message>
-        <source>Top/Left</source>
-        <translation>Links oben</translation>
-    </message>
-    <message>
-        <source>Top/Right</source>
-        <translation>Rechts oben</translation>
-    </message>
-    <message>
-        <source>Bottom/Left</source>
-        <translation>Links unten</translation>
-    </message>
-    <message>
-        <source>Bottom/Right</source>
-        <translation>Rechts unten</translation>
-    </message>
-    <message>
-        <source>Test Notification</source>
-        <translation>Testbenachrichtigung</translation>
-    </message>
-    <message>
+        <location filename="../basicsettings.cpp" line="57"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
 A fallback will be used.</source>
         <translation>&lt;b&gt;Warnung:&lt;/b&gt; Es läuft kein Benachrichtigungsdaemon.
 Ein Ersatz wird verwendet.</translation>
     </message>
     <message>
+        <location filename="../basicsettings.cpp" line="60"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
-These settings won&apos;t have an effect on it!</source>
+These settings won&apos;t have any effect on it!</source>
         <translation>&lt;b&gt;Warnung:&lt;/b&gt; Ein anderer Benachrichtigungsdaemon (%1) läuft.
 Diese Einstellungen wirken sich auf ihn nicht aus!</translation>
     </message>
     <message>
-        <source>Notification Summary</source>
-        <translation>Benachrichtigungszusammenfassung</translation>
+        <location filename="../basicsettings.cpp" line="117"/>
+        <source>Notification demo </source>
+        <translation>Testbenachrichtigung </translation>
     </message>
     <message>
-        <source>Notification Body</source>
-        <translation>Benachrichtigungsrumpf</translation>
+        <location filename="../basicsettings.cpp" line="118"/>
+        <source>This is a test notification.
+ All notifications will now appear here on LXQt.</source>
+        <translation>Dies ist eine Testbenachrichtigung.
+Alle Benachrichtigungen erscheinen jetzt hier auf LXQt.</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
+        <location filename="../mainwindow.cpp" line="40"/>
         <source>Desktop Notifications</source>
         <translation>Arbeitsflächenbenachrichtigungen</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="43"/>
         <source>Basic Settings</source>
         <translation>Grundeinstellungen</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="47"/>
         <source>Advanced Settings</source>
         <translation>Erweiterte Einstellungen</translation>
     </message>
