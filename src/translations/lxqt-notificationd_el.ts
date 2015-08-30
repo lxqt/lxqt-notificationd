3954b60c94ffbb6e1fe1f6c4d4f7228f8b9d215b
Dimitrios Glentadakis <dglent@free.fr>
2015-08-30 09:32:32 +0200

    Update Greek (el) translation Remove country variant from language code

---
+++ src/translations/lxqt-notificationd_el.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="el">
+<context>
+    <name>NotificationActionsComboWidget</name>
+    <message>
+        <source>Actions:</source>
+        <translation>Ενέργειες:</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>Εντάξει</translation>
+    </message>
+</context>
+</TS>
