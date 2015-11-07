0ff373b27a482b637df6654642712e3a5df11076
attuska <attus@enterpriseforever.com>
2015-11-07 11:48:46 +0100

    update lxqt-config-notificationd_hu.ts

--- config/translations/lxqt-config-notificationd_hu.ts
+++ config/translations/lxqt-config-notificationd_hu.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="hu_HU">
+<TS version="2.1" language="hu">
 <context>
     <name>AdvancedSettings</name>
     <message>
@@ -32,7 +32,7 @@
     <message>
         <location filename="../advancedsettings.ui" line="80"/>
         <source>Some notifications set their own on-screen duration.</source>
-        <translation>Egyes értesítések megjelenési ideje</translation>
+        <translation>Egyes értesítések megjelenési ideje.</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="92"/>
@@ -58,22 +58,6 @@
         <translation>Hely</translation>
     </message>
     <message>
-        <source>Top/Left</source>
-        <translation type="vanished">Balra fenn</translation>
-    </message>
-    <message>
-        <source>Top/Right</source>
-        <translation type="vanished">Jobbra fenn</translation>
-    </message>
-    <message>
-        <source>Bottom/Left</source>
-        <translation type="vanished">Balra lenn</translation>
-    </message>
-    <message>
-        <source>Bottom/Right</source>
-        <translation type="vanished">Jobbra lenn</translation>
-    </message>
-    <message>
         <location filename="../basicsettings.cpp" line="57"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
 A fallback will be used.</source>
@@ -84,29 +68,20 @@ A vésztartalék használatos.</translation>
         <location filename="../basicsettings.cpp" line="60"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
 These settings won&apos;t have any effect on it!</source>
-        <translation>&lt;b&gt;Figyelem:&lt;/b&gt; A (%1) démont már valaki futtatja.
+        <translation>&lt;b&gt;Figyelem:&lt;/b&gt; Egy másik, a (%1) értesítésfigyelő már fut.
 Ezek a beállítások arra már hatástalanok!</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="117"/>
         <source>Notification demo </source>
-        <translation type="unfinished"></translation>
+        <translation>Értesítés bemutató</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="118"/>
         <source>This is a test notification.
  All notifications will now appear here on LXQt.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Notification demo</source>
-        <translation type="vanished">Értesítési bemutató</translation>
-    </message>
-    <message>
-        <source>This is a test notification.
-All notifications will now appear here on LXQt.</source>
-        <translation type="vanished">Az értesítés tesztje
-Minden értesítést megjelenít az LXQt.</translation>
+        <translation>Értesítési teszt.
+ Az értesítéseket az LXQt így fogja megjeleníteni.</translation>
     </message>
 </context>
 <context>
