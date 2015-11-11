fd8bc93133a0459e23141b0ba9f90e00734c7172
Vincenzo Reale <smart2128@baslug.org>
2015-11-11 09:02:07 +0100

    Italian translation update

---
+++ src/translations/lxqt-notificationd_it.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it">
+<context>
+    <name>NotificationActionsComboWidget</name>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="101"/>
+        <source>Actions:</source>
+        <translation>Azioni:</translation>
+    </message>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="120"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+</TS>
