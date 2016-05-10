824ef92ae7d0f381b5d7ece890a4a2f161a02a8a
Alf Gaida <agaida@siduction.org>
2016-05-10 19:06:39 +0200

    Some renaming
    Renamed config/translations into lxqt-config-notificationd
    REnamed src/translations into lxqt-notificationd

---
+++ lxqt-notificationd_el.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="el">
+<context>
+    <name>NotificationActionsComboWidget</name>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="101"/>
+        <source>Actions:</source>
+        <translation>Ενέργειες:</translation>
+    </message>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="120"/>
+        <source>OK</source>
+        <translation>Εντάξει</translation>
+    </message>
+</context>
+</TS>
