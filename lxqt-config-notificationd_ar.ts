635d3e93202b6fe3882efbc01269252c2baa4c5a
safaalfulaij <safa1996alfulaij@gmail.com>
2016-07-08 17:33:08 +0300

    Update Arabic translations

---
+++ lxqt-config-notificationd_ar.ts
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ar">
+<context>
+    <name>AdvancedSettings</name>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="17"/>
+        <source>Sizes</source>
+        <translation>الأحجام</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="25"/>
+        <source>Width:</source>
+        <translation>العرض:</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="32"/>
+        <location filename="../../../advancedsettings.ui" line="56"/>
+        <source> px</source>
+        <translation> بكسل</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="49"/>
+        <source>Spacing:</source>
+        <translation>التّباعد:</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="68"/>
+        <source>Duration</source>
+        <translation>المدّة</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="80"/>
+        <source>Some notifications set their own on-screen duration.</source>
+        <translation>بعض الإخطارات تضبط مدّتها على الشّاشة بنفسها.</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="92"/>
+        <source>Default duration:</source>
+        <translation>المدّة الافتراضيّة:</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="99"/>
+        <source> sec</source>
+        <translation> ثا</translation>
+    </message>
+</context>
+<context>
+    <name>BasicSettings</name>
+    <message>
+        <location filename="../../../basicsettings.ui" line="23"/>
+        <source>Basic Settings</source>
+        <translation>الإعدادات الأساسيّة</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.ui" line="30"/>
+        <source>Position on screen</source>
+        <translation>المكان على الشّاشة</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="57"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
+A fallback will be used.</source>
+        <translation>&lt;b&gt;تحذير:&lt;/b&gt; لا عفريت إخطارات يعمل.
+سأستخدم أحد الاحتياطيّين.</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="60"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
+These settings won&apos;t have any effect on it!</source>
+        <translation>&lt;b&gt;تحذير:&lt;/b&gt; هناك عفريت من طرف ثالث (%1) يعمل.
+لن تؤثّر هذه الإعدادات عليه!</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="117"/>
+        <source>Notification demo </source>
+        <translation>نموذج لإخطار</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="118"/>
+        <source>This is a test notification.
+ All notifications will now appear here on LXQt.</source>
+        <translation>هذا إخطار تجريبيّ.
+ستظهر كلّ الإخطارات هنا في لكسكيوت.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="40"/>
+        <source>Desktop Notifications</source>
+        <translation>إخطارات سطح المكتب</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="43"/>
+        <source>Basic Settings</source>
+        <translation>الإعدادات الأساسيّة</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="47"/>
+        <source>Advanced Settings</source>
+        <translation>الإعدادات المتقدّمة</translation>
+    </message>
+</context>
+</TS>
