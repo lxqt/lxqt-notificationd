23afd6a996b105e781ee86f19d4c6f364dab2cc2
Pavel Fric <pavelfric@yahoo.com>
2016-08-16 06:54:50 +0200

    Add/Update Czech translations

---
+++ lxqt-config-notificationd_cs.ts
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
+<context>
+    <name>AdvancedSettings</name>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="17"/>
+        <source>Sizes</source>
+        <translation>Velikosti</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="25"/>
+        <source>Width:</source>
+        <translation>Šířka:</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="32"/>
+        <location filename="../../../advancedsettings.ui" line="56"/>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="49"/>
+        <source>Spacing:</source>
+        <translation>Rozestupy: </translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="68"/>
+        <source>Duration</source>
+        <translation>Doba trvání</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="80"/>
+        <source>Some notifications set their own on-screen duration.</source>
+        <translation>Některá oznámení si nastavují svoji vlastní dobu trvání zobrazení.</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="92"/>
+        <source>Default duration:</source>
+        <translation>Výchozí doba trvání: </translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="99"/>
+        <source> sec</source>
+        <translation> s</translation>
+    </message>
+</context>
+<context>
+    <name>BasicSettings</name>
+    <message>
+        <location filename="../../../basicsettings.ui" line="23"/>
+        <source>Basic Settings</source>
+        <translation>Základní nastavení</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.ui" line="30"/>
+        <source>Position on screen</source>
+        <translation>Poloha na obrazovce</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="57"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
+A fallback will be used.</source>
+        <translation>&lt;b&gt;Varování:&lt;/b&gt; Neběží žádný oznamovací démon.
+Použije se náhrada.</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="60"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
+These settings won&apos;t have any effect on it!</source>
+        <translation>&lt;b&gt;Varování:&lt;/b&gt; Běží jiný oznamovací démon (od třetí strany) (%1).
+Tato nastavení na něj nemají žádný vliv!</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="117"/>
+        <source>Notification demo </source>
+        <translation>Zkušební oznámení</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="118"/>
+        <source>This is a test notification.
+ All notifications will now appear here on LXQt.</source>
+        <translation>Toto je zkušební oznámení.
+Všechna oznámení se nyní budou objevovat zde na LXQt.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="40"/>
+        <source>Desktop Notifications</source>
+        <translation>Oznámení na plochu</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="43"/>
+        <source>Basic Settings</source>
+        <translation>Základní nastavení</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="47"/>
+        <source>Advanced Settings</source>
+        <translation>Pokročilá nastavení</translation>
+    </message>
+</context>
+</TS>
