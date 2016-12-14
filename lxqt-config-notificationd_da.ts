3d72d74fdb1af6ff20c0d2188c72bda1a02d632c
scootergrisen <scootergrisen@gmail.com>
2016-12-14 02:46:15 +0100

    Update Danish translations

---
+++ lxqt-config-notificationd_da.ts
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="da">
+<context>
+    <name>AdvancedSettings</name>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="17"/>
+        <source>Sizes</source>
+        <translation>Størrelser</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="25"/>
+        <source>Width:</source>
+        <translation>Bredde:</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="32"/>
+        <location filename="../../../advancedsettings.ui" line="56"/>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="49"/>
+        <source>Spacing:</source>
+        <translation>Afstand:</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="68"/>
+        <source>Duration</source>
+        <translation>Varighed</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="80"/>
+        <source>Some notifications set their own on-screen duration.</source>
+        <translation>Nogle notifikationer sætter deres egen varighed for hvor længe de blive vist på skærmen.</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="92"/>
+        <source>Default duration:</source>
+        <translation>Standardvarighed:</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="99"/>
+        <source> sec</source>
+        <translation> sek.</translation>
+    </message>
+</context>
+<context>
+    <name>BasicSettings</name>
+    <message>
+        <location filename="../../../basicsettings.ui" line="23"/>
+        <source>Basic Settings</source>
+        <translation>Grundlæggende indstillinger</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.ui" line="30"/>
+        <source>Position on screen</source>
+        <translation>Placering på skærmen</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="57"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
+A fallback will be used.</source>
+        <translation>&lt;b&gt;Advarsel:&lt;/b&gt; der kører ikke noget notifikationsbaggrundsprogram.
+Et tilbagefald vil blive brugt.</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="60"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
+These settings won&apos;t have any effect on it!</source>
+        <translation>&lt;b&gt;Advarsel:&lt;/b&gt; der kører et tredjeparts notifikationsbaggrundsprogram (%1).
+Disse indstillinger vil ikke have nogen effekt på det!</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="117"/>
+        <source>Notification demo </source>
+        <translation>Notifikationsdemo </translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="118"/>
+        <source>This is a test notification.
+ All notifications will now appear here on LXQt.</source>
+        <translation>Dette er en test-notifikation.
+ Alle notifikationer vises her i LXQt.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="40"/>
+        <source>Desktop Notifications</source>
+        <translation>Skrivebordsnotifikationer</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="43"/>
+        <source>Basic Settings</source>
+        <translation>Grundlæggende indstillinger</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="47"/>
+        <source>Advanced Settings</source>
+        <translation>Avanceret indstillinger</translation>
+    </message>
+</context>
+</TS>
