e0dadd35a7f1d9db6c861930e6c32e9f999664db
Toms Tilgalis <ttilgalis@gmail.com>
2017-09-06 04:29:50 +0300

    Latvian translations

---
+++ lxqt-config-notificationd_lv.ts
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lv">
+<context>
+    <name>AdvancedSettings</name>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="17"/>
+        <source>Sizes</source>
+        <translation>Izmēri</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="25"/>
+        <source>Width:</source>
+        <translation>Platums:</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="32"/>
+        <location filename="../../../advancedsettings.ui" line="56"/>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="49"/>
+        <source>Spacing:</source>
+        <translation>Atstarpe:</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="68"/>
+        <source>Duration</source>
+        <translation>Ilgums</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="80"/>
+        <source>Some notifications set their own on-screen duration.</source>
+        <translation>Daži paziņōjumi uzstāda paši savu rādīšanas ilgumu.</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="92"/>
+        <source>Default duration:</source>
+        <translation>Noklusējuma ilgums:</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="99"/>
+        <source> sec</source>
+        <translation> sek</translation>
+    </message>
+</context>
+<context>
+    <name>BasicSettings</name>
+    <message>
+        <location filename="../../../basicsettings.ui" line="23"/>
+        <source>Basic Settings</source>
+        <translation>Pamatiestatījumi</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.ui" line="30"/>
+        <source>Position on screen</source>
+        <translation>Novietojums uz ekrāna</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="57"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
+A fallback will be used.</source>
+        <translation>&lt;b&gt;Brīdinājums:&lt;/b&gt; Paziņojumu process nav palaists. 
+Tiks lietots aizstājējs.</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="60"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
+These settings won&apos;t have any effect on it!</source>
+        <translation>&lt;b&gt;Brīdinājums:&lt;/b&gt;Darbojas paziņojumu process (%1).
+Šiem iestatījumiem nebūs rezultāta!</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="117"/>
+        <source>Notification demo </source>
+        <translation>Paziņojuma demonstrācija</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="118"/>
+        <source>This is a test notification.
+ All notifications will now appear here on LXQt.</source>
+        <translation>Šis ir paraugdemonstrējums.
+ LXQt paziņojumi tiks rādīti šeit.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="40"/>
+        <source>Desktop Notifications</source>
+        <translation>Darbvirsmas paziņojumi</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="43"/>
+        <source>Basic Settings</source>
+        <translation>Pamatiestatījumi</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="47"/>
+        <source>Advanced Settings</source>
+        <translation>Papildus iestatījumi</translation>
+    </message>
+</context>
+</TS>
