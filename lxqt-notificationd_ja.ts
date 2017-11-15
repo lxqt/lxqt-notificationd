bd91968a1db437df6969c0205cbbe8823f7f148d
Takefumi Nagata <rago1975@users.noreply.github.com>
2017-11-16 00:51:37 +0900

    Update Japanese translations (#303)
    * Japanese translation updated
    
    * Correct some Japanese translation

---
+++ lxqt-notificationd_ja.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja">
+<context>
+    <name>NotificationActionsComboWidget</name>
+    <message>
+        <location filename="../../../notificationwidgets.cpp" line="101"/>
+        <source>Actions:</source>
+        <translation>動作:</translation>
+    </message>
+    <message>
+        <location filename="../../../notificationwidgets.cpp" line="120"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
