5a919c07662a030775c096b5d68dc449a3ad7f21
yo <yo@ejemplo.com>
2018-04-28 16:01:42 +0200

    Complete Spanish translation

---
+++ lxqt-config-notificationd_es.ts
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES" sourcelanguage="es">
+<context>
+    <name>AdvancedSettings</name>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="17"/>
+        <source>Sizes</source>
+        <translation>Medidas</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="25"/>
+        <source>Width:</source>
+        <translation>Ancho:</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="32"/>
+        <location filename="../../../advancedsettings.ui" line="56"/>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="49"/>
+        <source>Spacing:</source>
+        <translation>Espaciado:</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="68"/>
+        <source>Duration</source>
+        <translation>Duración</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="80"/>
+        <source>Some notifications set their own on-screen duration.</source>
+        <translation>Algunas notificaciones establecen su propia duracion en pantalla.</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="92"/>
+        <source>Default duration:</source>
+        <translation>Duración por defecto:</translation>
+    </message>
+    <message>
+        <location filename="../../../advancedsettings.ui" line="99"/>
+        <source> sec</source>
+        <translation> segundos</translation>
+    </message>
+</context>
+<context>
+    <name>BasicSettings</name>
+    <message>
+        <location filename="../../../basicsettings.ui" line="23"/>
+        <source>Basic Settings</source>
+        <translation>Configuración básica</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.ui" line="30"/>
+        <source>Position on screen</source>
+        <translation>Posición en pantalla</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="56"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
+A fallback will be used.</source>
+        <translation>&lt;b&gt;Aviso:&lt;/b&gt; no hay ningún demonio de notificaciones en ejecución.
+Se usará un medio alternativo.</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="59"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
+These settings won&apos;t have any effect on it!</source>
+        <translation>&lt;b&gt;Aviso:&lt;/b&gt; un demonio de notificaciones de terceros (%1) está en ejecución.
+¡Estos ajustes no tendrán ningún efecto sobre él!</translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="112"/>
+        <source>Notification demo </source>
+        <translation>Prueba de notificación </translation>
+    </message>
+    <message>
+        <location filename="../../../basicsettings.cpp" line="113"/>
+        <source>This is a test notification.
+ All notifications will now appear here on LXQt.</source>
+        <translation>Esto es una notificación de prueba
+ Todas las notificaciones apareceran desde ahora aquí en LXQt.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="40"/>
+        <source>Desktop Notifications</source>
+        <translation>Notificaciones del escritorio</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="43"/>
+        <source>Basic Settings</source>
+        <translation>Configuración básica</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="47"/>
+        <source>Advanced Settings</source>
+        <translation>Configuración avanzada</translation>
+    </message>
+</context>
+</TS>
