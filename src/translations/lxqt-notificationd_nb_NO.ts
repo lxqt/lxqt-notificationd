b26088437d04309175d0652b19f818541c6a71c1
Einar Mostad <einar@mostad.eu>
2018-10-24 18:41:04 +0000

    Added translation using Weblate (Norwegian Bokmål)

---
+++ src/translations/lxqt-notificationd_nb_NO.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb_NO">
+<context>
+    <name>NotificationActionsComboWidget</name>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="103"/>
+        <source>Actions:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="122"/>
+        <source>OK</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
