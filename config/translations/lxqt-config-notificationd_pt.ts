a574237424db2a33a582c303278170bc3a6743b3
eltonfabricio10 <eltonfabricio10@gmail.com>
2018-11-05 14:29:15 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (17 of 17 strings)
    
    Translation: LXQt/LXQt Notification Daemon (Config)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-notificationd/pt/

--- config/translations/lxqt-config-notificationd_pt.ts
+++ config/translations/lxqt-config-notificationd_pt.ts
@@ -21,7 +21,7 @@
     <message>
         <location filename="../advancedsettings.ui" line="80"/>
         <source>Some notifications set their own on-screen duration.</source>
-        <translation>Algumas notificações definem a sua duração no ecrã.</translation>
+        <translation>Algumas notificações definem sua própria duração na tela.</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="92"/>
@@ -50,38 +50,38 @@
     <message>
         <location filename="../basicsettings.ui" line="23"/>
         <source>Basic Settings</source>
-        <translation>Definições básicas</translation>
+        <translation>Configurações Básicas</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="30"/>
         <source>Position on screen</source>
-        <translation>Posição no ecrã</translation>
+        <translation>Posição na tela</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="56"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
 A fallback will be used.</source>
-        <translation>&lt;b&gt;Aviso:&lt;/b&gt; o serviço de notificaçoes não está em execução.
-Será utilizada uma solução alternativa.</translation>
+        <translation>&lt;b&gt;Atenção:&lt;/b&gt;Nenhum serviço de notificações está sendo executado.
+Será usada uma solução alternativa.</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="59"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
 These settings won&apos;t have any effect on it!</source>
-        <translation>&lt;b&gt;Aviso:&lt;/b&gt;já está a ser executado o serviço de notificações (%1).
-Estas definições não produzirão qualquer efeito!</translation>
+        <translation>&lt;b&gt;Atenção:&lt;/b&gt;Um serviço de notificações de terceiros (%1) está sendo executado.
+Essas configurações não terão nenhum efeito sobre isso!</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="112"/>
         <source>Notification demo </source>
-        <translation>Demonstração </translation>
+        <translation>Demonstração de notificação </translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="113"/>
         <source>This is a test notification.
  All notifications will now appear here on LXQt.</source>
         <translation>Esta é uma notificação de teste.
-Todas as suas notificações aparecerão aqui.</translation>
+ Todas as notificações aparecerão agora aqui no LXQt.</translation>
     </message>
 </context>
 <context>
@@ -89,17 +89,17 @@ Todas as suas notificações aparecerão aqui.</translation>
     <message>
         <location filename="../mainwindow.cpp" line="41"/>
         <source>Desktop Notifications</source>
-        <translation>Notificações do ambiente de trabalho</translation>
+        <translation>Notificações da Área de Trabalho</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="44"/>
         <source>Basic Settings</source>
-        <translation>Definições básicas</translation>
+        <translation>Configurações Básicas</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="48"/>
         <source>Advanced Settings</source>
-        <translation>Definições avançadas</translation>
+        <translation>Configurações Avançadas</translation>
     </message>
 </context>
 </TS>
