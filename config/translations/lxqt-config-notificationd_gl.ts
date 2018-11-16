ae30030fb85c0c2a789c0b5dc4d26dfce69c746c
Miguel A. Bouzada <mbouzada@gmail.com>
2018-11-16 11:25:20 +0000

    Translated using Weblate (Galician)
    Currently translated at 100.0% (17 of 17 strings)
    
    Translation: LXQt/LXQt Notification Daemon (Config)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-notificationd/gl/

--- config/translations/lxqt-config-notificationd_gl.ts
+++ config/translations/lxqt-config-notificationd_gl.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../advancedsettings.ui" line="17"/>
         <source>Sizes</source>
-        <translation></translation>
+        <translation>Medidas</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="25"/>
         <source>Width:</source>
-        <translation></translation>
+        <translation>Largo:</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="32"/>
@@ -22,27 +22,27 @@
     <message>
         <location filename="../advancedsettings.ui" line="49"/>
         <source>Spacing:</source>
-        <translation></translation>
+        <translation>Espazado:</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="68"/>
         <source>Duration</source>
-        <translation></translation>
+        <translation>Duración</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="80"/>
         <source>Some notifications set their own on-screen duration.</source>
-        <translation></translation>
+        <translation>Algunhas notificacións estabelecen a súa propia duración en pantalla.</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="92"/>
         <source>Default duration:</source>
-        <translation></translation>
+        <translation>Duración por omisión:</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="99"/>
         <source> sec</source>
-        <translation></translation>
+        <translation> seg</translation>
     </message>
 </context>
 <context>
@@ -50,35 +50,38 @@
     <message>
         <location filename="../basicsettings.ui" line="23"/>
         <source>Basic Settings</source>
-        <translation></translation>
+        <translation>Axustes básicos</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="30"/>
         <source>Position on screen</source>
-        <translation></translation>
+        <translation>Posición na pantalla</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="56"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
 A fallback will be used.</source>
-        <translation></translation>
+        <translation>&lt;b&gt;Aviso:&lt;/b&gt; Non hai ningún servizo de notificación en execución.
+Empregarase un método alternativo.</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="59"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
 These settings won&apos;t have any effect on it!</source>
-        <translation></translation>
+        <translation>&lt;b&gt;Aviso:&lt;/b&gt; Hai un servizo de notificacións de terceiros (%1) en execución.
+Estes axustes non lle afectarán!</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="112"/>
         <source>Notification demo </source>
-        <translation></translation>
+        <translation>Proba de notificación </translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="113"/>
         <source>This is a test notification.
  All notifications will now appear here on LXQt.</source>
-        <translation></translation>
+        <translation>Isto é unha notificación de proba.
+ A partires de agora, todas as notificacións aparecerán aquí en LXQt.</translation>
     </message>
 </context>
 <context>
@@ -86,17 +89,17 @@ These settings won&apos;t have any effect on it!</source>
     <message>
         <location filename="../mainwindow.cpp" line="41"/>
         <source>Desktop Notifications</source>
-        <translation></translation>
+        <translation>Notificacións do escritorio</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="44"/>
         <source>Basic Settings</source>
-        <translation></translation>
+        <translation>Axustes básicos</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="48"/>
         <source>Advanced Settings</source>
-        <translation></translation>
+        <translation>Axustes avanzados</translation>
     </message>
 </context>
 </TS>
