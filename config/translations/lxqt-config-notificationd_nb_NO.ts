114f4df5b696e13585c0a5246661c21a0e952c6b
Einar Mostad <einar@mostad.eu>
2018-11-19 23:35:09 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (17 of 17 strings)
    
    Translation: LXQt/LXQt Notification Daemon (Config)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-notificationd/nb_NO/

--- config/translations/lxqt-config-notificationd_nb_NO.ts
+++ config/translations/lxqt-config-notificationd_nb_NO.ts
@@ -6,43 +6,43 @@
     <message>
         <location filename="../advancedsettings.ui" line="17"/>
         <source>Sizes</source>
-        <translation></translation>
+        <translation>Størrelser</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="25"/>
         <source>Width:</source>
-        <translation></translation>
+        <translation>Bredde:</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="32"/>
         <location filename="../advancedsettings.ui" line="56"/>
         <source> px</source>
-        <translation></translation>
+        <translation> piksler</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="49"/>
         <source>Spacing:</source>
-        <translation></translation>
+        <translation>Avstand:</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="68"/>
         <source>Duration</source>
-        <translation></translation>
+        <translation>Varighet</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="80"/>
         <source>Some notifications set their own on-screen duration.</source>
-        <translation></translation>
+        <translation>Noen varsler bestemmer selv hvor lenge de vises.</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="92"/>
         <source>Default duration:</source>
-        <translation></translation>
+        <translation>Standard varighet:</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="99"/>
         <source> sec</source>
-        <translation></translation>
+        <translation> sekunder</translation>
     </message>
 </context>
 <context>
@@ -50,35 +50,38 @@
     <message>
         <location filename="../basicsettings.ui" line="23"/>
         <source>Basic Settings</source>
-        <translation></translation>
+        <translation>Grunnleggende innstillinger</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="30"/>
         <source>Position on screen</source>
-        <translation></translation>
+        <translation>Plassering på skjermen</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="56"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
 A fallback will be used.</source>
-        <translation></translation>
+        <translation>&lt;b&gt;Advarsel:&lt;/b&gt; Ingen daemon for varsler kjører.
+En nødløsning vil brukes.</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="59"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
 These settings won&apos;t have any effect on it!</source>
-        <translation></translation>
+        <translation>&lt;b&gt;Advarsel:&lt;/b&gt; En tredjeparts daemon (%1) kjører.
+Disse innstillingene har ingen effekt på den!</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="112"/>
         <source>Notification demo </source>
-        <translation></translation>
+        <translation>Varselsdemonstrasjon </translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="113"/>
         <source>This is a test notification.
  All notifications will now appear here on LXQt.</source>
-        <translation></translation>
+        <translation>Dette er et testvarsel.
+ Alle varsler vil nå vises her på LXQt.</translation>
     </message>
 </context>
 <context>
@@ -86,17 +89,17 @@ These settings won&apos;t have any effect on it!</source>
     <message>
         <location filename="../mainwindow.cpp" line="41"/>
         <source>Desktop Notifications</source>
-        <translation></translation>
+        <translation>Skrivebordsvarsler</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="44"/>
         <source>Basic Settings</source>
-        <translation></translation>
+        <translation>Grunnleggende innstillinger</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="48"/>
         <source>Advanced Settings</source>
-        <translation></translation>
+        <translation>Avanserte innstillinger</translation>
     </message>
 </context>
 </TS>
