eb3834cc97a445019788218e89f68859b580163c
Alf Gaida <agaida@siduction.org>
2018-12-10 23:17:26 +0100

    translation update

--- config/translations/lxqt-config-notificationd_it.ts
+++ config/translations/lxqt-config-notificationd_it.ts
@@ -34,6 +34,32 @@
         <translation> sec</translation>
     </message>
     <message>
+        <location filename="../advancedsettings.ui" line="127"/>
+        <source>Unattended Notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="135"/>
+        <source>How many to save:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="149"/>
+        <location filename="../advancedsettings.ui" line="159"/>
+        <source>Application name is on the top of notification.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="152"/>
+        <source>Ignore these applications:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="162"/>
+        <source>app1,app2,app3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="../advancedsettings.ui" line="17"/>
         <source>Sizes</source>
         <translation>Dimensioni</translation>
