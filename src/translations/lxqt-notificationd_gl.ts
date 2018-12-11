007793c582280c18c86202f435f96ea0124c9b6e
Miguel A. Bouzada <mbouzada@gmail.com>
2018-12-11 10:35:26 +0000

    Translated using Weblate (Galician)
    Currently translated at 100,0% (5 of 5 strings)
    
    Translation: LXQt/LXQt Notification Daemon
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-notificationd/gl/

--- src/translations/lxqt-notificationd_gl.ts
+++ src/translations/lxqt-notificationd_gl.ts
@@ -19,19 +19,19 @@
     <message>
         <location filename="../notifyd.cpp" line="226"/>
         <source>Clear All</source>
-        <translation type="unfinished"></translation>
+        <translation>Limpar todo</translation>
     </message>
     <message>
         <location filename="../notifyd.cpp" line="234"/>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Opcións</translation>
     </message>
     <message>
         <location filename="../notifyd.cpp" line="241"/>
         <location filename="../notifyd.cpp" line="298"/>
         <location filename="../notifyd.cpp" line="318"/>
         <source>%1 Unattended Notification(s)</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 notificación/s) desatendida(s)</translation>
     </message>
 </context>
 </TS>
