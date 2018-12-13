ed75a09528050b0be6873a7f918821e3aaa8a76f
micrococo <micrococo@gmx.com>
2018-12-13 13:59:24 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (22 of 22 strings)
    
    Translation: LXQt/LXQt Notification Daemon (Config)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-notificationd/es/

--- config/translations/lxqt-config-notificationd_es.ts
+++ config/translations/lxqt-config-notificationd_es.ts
@@ -47,28 +47,28 @@
     <message>
         <location filename="../advancedsettings.ui" line="127"/>
         <source>Unattended Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Notificaciones desatendidas</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="135"/>
         <source>How many to save:</source>
-        <translation type="unfinished"></translation>
+        <translation>Cuántas se guardan:</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="149"/>
         <location filename="../advancedsettings.ui" line="159"/>
         <source>Application name is on the top of notification.</source>
-        <translation type="unfinished"></translation>
+        <translation>El nombre de la aplicación está en la parte superior de la notificación.</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="152"/>
         <source>Ignore these applications:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ignorar estas aplicaciones:</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="162"/>
         <source>app1,app2,app3</source>
-        <translation type="unfinished"></translation>
+        <translation>aplicación1,aplicación2,aplicación3</translation>
     </message>
 </context>
 <context>
