e33d511e514f1c6b1cef730659561376317b9d24
Takefumi Nagata <take1975@kumamoto.email.ne.jp>
2019-01-22 14:41:04 +0000

    Translated using Weblate (Japanese)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: LXQt/LXQt Notification Daemon
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-notificationd/ja/

--- src/translations/lxqt-notificationd_ja.ts
+++ src/translations/lxqt-notificationd_ja.ts
@@ -19,19 +19,19 @@
     <message>
         <location filename="../notifyd.cpp" line="226"/>
         <source>Clear All</source>
-        <translation type="unfinished"></translation>
+        <translation>全てクリア</translation>
     </message>
     <message>
         <location filename="../notifyd.cpp" line="234"/>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>オプション</translation>
     </message>
     <message>
         <location filename="../notifyd.cpp" line="241"/>
         <location filename="../notifyd.cpp" line="298"/>
         <location filename="../notifyd.cpp" line="318"/>
         <source>%1 Unattended Notification(s)</source>
-        <translation type="unfinished"></translation>
+        <translation>%1個の未読通知</translation>
     </message>
 </context>
 </TS>
