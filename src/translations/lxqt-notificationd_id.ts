6123d8f303b23210e54ee83e7b733ab3119d467d
Nabil <nblid48@gmail.com>
2019-02-05 01:41:27 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 80.0% (4 of 5 strings)
    
    Translation: LXQt/LXQt Notification Daemon
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-notificationd/id/

--- src/translations/lxqt-notificationd_id.ts
+++ src/translations/lxqt-notificationd_id.ts
@@ -19,12 +19,12 @@
     <message>
         <location filename="../notifyd.cpp" line="226"/>
         <source>Clear All</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Tutup Semua</translation>
     </message>
     <message>
         <location filename="../notifyd.cpp" line="234"/>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Plihan</translation>
     </message>
     <message>
         <location filename="../notifyd.cpp" line="241"/>
