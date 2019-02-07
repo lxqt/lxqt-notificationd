5578742cec195b806c4b55f2c0861f215c9b8011
Gema Aji Wardian <gema_wardian@hotmail.com>
2019-02-07 11:19:23 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: LXQt/LXQt Notification Daemon
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-notificationd/id/

--- src/translations/lxqt-notificationd_id.ts
+++ src/translations/lxqt-notificationd_id.ts
@@ -19,7 +19,7 @@
     <message>
         <location filename="../notifyd.cpp" line="226"/>
         <source>Clear All</source>
-        <translation type="unfinished">Tutup Semua</translation>
+        <translation>Bersihkan Semua</translation>
     </message>
     <message>
         <location filename="../notifyd.cpp" line="234"/>
