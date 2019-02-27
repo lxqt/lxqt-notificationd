9a3e331c0b260d6923bb3121007fa3315e1f13b9
Kochih Wu <mecca831@gmail.com>
2019-02-27 02:31:18 +0000

    Translated using Weblate (Chinese (Traditional))
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: LXQt/LXQt Notification Daemon
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-notificationd/zh_TW/

--- src/translations/lxqt-notificationd_zh_TW.ts
+++ src/translations/lxqt-notificationd_zh_TW.ts
@@ -19,19 +19,19 @@
     <message>
         <location filename="../notifyd.cpp" line="226"/>
         <source>Clear All</source>
-        <translation type="unfinished"></translation>
+        <translation>全部清除</translation>
     </message>
     <message>
         <location filename="../notifyd.cpp" line="234"/>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>選項</translation>
     </message>
     <message>
         <location filename="../notifyd.cpp" line="241"/>
         <location filename="../notifyd.cpp" line="298"/>
         <location filename="../notifyd.cpp" line="318"/>
         <source>%1 Unattended Notification(s)</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 個未讀通知</translation>
     </message>
 </context>
 </TS>
