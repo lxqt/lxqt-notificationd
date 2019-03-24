bf15ede6a965af06b531c178ed2d92cb0544e676
Mark Clarkstone <hello@markclarkstone.co.uk>
2019-03-24 03:14:40 +0000

    Added translation using Weblate (English (United Kingdom))

---
+++ src/translations/lxqt-notificationd_en_GB.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_GB">
+<context>
+    <name>NotificationActionsComboWidget</name>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="103"/>
+        <source>Actions:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="122"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Notifyd</name>
+    <message>
+        <location filename="../notifyd.cpp" line="226"/>
+        <source>Clear All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notifyd.cpp" line="234"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notifyd.cpp" line="241"/>
+        <location filename="../notifyd.cpp" line="298"/>
+        <location filename="../notifyd.cpp" line="318"/>
+        <source>%1 Unattended Notification(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
