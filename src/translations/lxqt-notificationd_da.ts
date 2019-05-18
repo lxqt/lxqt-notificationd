3e17b37f7dcb2d43fc83be97979f4e77eafdab15
scootergrisen <scootergrisen@gmail.com>
2019-05-18 22:08:31 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: LXQt/LXQt Notification Daemon
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-notificationd/da/

--- src/translations/lxqt-notificationd_da.ts
+++ src/translations/lxqt-notificationd_da.ts
@@ -31,7 +31,7 @@
         <location filename="../notifyd.cpp" line="298"/>
         <location filename="../notifyd.cpp" line="318"/>
         <source>%1 Unattended Notification(s)</source>
-        <translation>%1 notifikation(er) uden opsyn</translation>
+        <translation>%1 underretning(er) uden opsyn</translation>
     </message>
 </context>
 </TS>
