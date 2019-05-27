612bb1166b415a74be1b693c3f86187f43748ee5
Paolo Vigoroso <xpaolox@zoznam.sk>
2019-05-27 11:35:17 +0000

    Translated using Weblate (Slovak)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: LXQt/LXQt Notification Daemon
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-notificationd/sk_SK/

--- src/translations/lxqt-notificationd_sk_SK.ts
+++ src/translations/lxqt-notificationd_sk_SK.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../notificationwidgets.cpp" line="103"/>
         <source>Actions:</source>
-        <translation type="unfinished"></translation>
+        <translation>Akcia:</translation>
     </message>
     <message>
         <location filename="../notificationwidgets.cpp" line="122"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>OK</translation>
     </message>
 </context>
 <context>
@@ -19,19 +19,19 @@
     <message>
         <location filename="../notifyd.cpp" line="226"/>
         <source>Clear All</source>
-        <translation type="unfinished"></translation>
+        <translation>Vyčistiť všetko</translation>
     </message>
     <message>
         <location filename="../notifyd.cpp" line="234"/>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Nastavenia</translation>
     </message>
     <message>
         <location filename="../notifyd.cpp" line="241"/>
         <location filename="../notifyd.cpp" line="298"/>
         <location filename="../notifyd.cpp" line="318"/>
         <source>%1 Unattended Notification(s)</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 nevidených oznámení</translation>
     </message>
 </context>
 </TS>
