b745ca1fc0ab24a3ec369a5e4862b6fb50a628fa
p-bo <pavel.borecki@gmail.com>
2019-09-27 05:53:52 +0000

    Translated using Weblate (Czech)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: LXQt/LXQt Notification Daemon
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-notificationd/cs/

--- src/translations/lxqt-notificationd_cs.ts
+++ src/translations/lxqt-notificationd_cs.ts
@@ -31,10 +31,10 @@
         <location filename="../notifyd.cpp" line="299"/>
         <location filename="../notifyd.cpp" line="319"/>
         <source>%n Unattended Notification(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n nepřečtené oznámení</numerusform>
+            <numerusform>%n nepřečtená oznámení</numerusform>
+            <numerusform>%n nepřečtených oznámení</numerusform>
         </translation>
     </message>
 </context>
