b49942ba390342ef1574ec80892316daa1dc8ffc
Miguel A. Bouzada <mbouzada@gmail.com>
2019-09-27 09:21:45 +0000

    Translated using Weblate (Galician)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: LXQt/LXQt Notification Daemon
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-notificationd/gl/

--- src/translations/lxqt-notificationd_gl.ts
+++ src/translations/lxqt-notificationd_gl.ts
@@ -31,9 +31,9 @@
         <location filename="../notifyd.cpp" line="299"/>
         <location filename="../notifyd.cpp" line="319"/>
         <source>%n Unattended Notification(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n notificación desatendida</numerusform>
+            <numerusform>%n notificacións desatendidas</numerusform>
         </translation>
     </message>
 </context>
