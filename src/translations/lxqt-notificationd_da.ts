8dd0de62bc4d4bd35218929893aa280adb3159d5
scootergrisen <scootergrisen@gmail.com>
2019-10-01 01:19:55 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: LXQt/LXQt Notification Daemon
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-notificationd/da/

--- src/translations/lxqt-notificationd_da.ts
+++ src/translations/lxqt-notificationd_da.ts
@@ -31,9 +31,9 @@
         <location filename="../notifyd.cpp" line="299"/>
         <location filename="../notifyd.cpp" line="319"/>
         <source>%n Unattended Notification(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n ikke-tilsete underretning</numerusform>
+            <numerusform>%n ikke-tilsete underretninger</numerusform>
         </translation>
     </message>
 </context>
