1caa2d7c74110897c5ceef56e05ccedc702e9f76
Sérgio Marques <smarquespt@gmail.com>
2019-10-06 22:37:36 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: LXQt/LXQt Notification Daemon
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-notificationd/pt/

--- src/translations/lxqt-notificationd_pt.ts
+++ src/translations/lxqt-notificationd_pt.ts
@@ -31,9 +31,9 @@
         <location filename="../notifyd.cpp" line="299"/>
         <location filename="../notifyd.cpp" line="319"/>
         <source>%n Unattended Notification(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n notificação pendente</numerusform>
+            <numerusform>%n notificações pendentes</numerusform>
         </translation>
     </message>
 </context>
