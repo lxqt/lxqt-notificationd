79bc4836d342aed4b909ecbf264e9d08f282b226
Oben Devin Çetiner <obendevin@gmail.com>
2019-11-01 16:46:05 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: LXQt/LXQt Notification Daemon
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-notificationd/tr/

--- src/translations/lxqt-notificationd_tr.ts
+++ src/translations/lxqt-notificationd_tr.ts
@@ -31,8 +31,9 @@
         <location filename="../notifyd.cpp" line="299"/>
         <location filename="../notifyd.cpp" line="319"/>
         <source>%n Unattended Notification(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n Görülmemiş Bildirim</numerusform>
+            <numerusform>%n Görülmemiş Bildirimler</numerusform>
         </translation>
     </message>
 </context>
