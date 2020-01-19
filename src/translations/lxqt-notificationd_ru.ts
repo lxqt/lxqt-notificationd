8c14740d5660ec659c7d8e08c421a7808a6f8225
Golubev Alexander <fatzer2@gmail.com>
2020-01-19 23:37:38 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: LXQt/LXQt Notification Daemon
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-notificationd/ru/

--- src/translations/lxqt-notificationd_ru.ts
+++ src/translations/lxqt-notificationd_ru.ts
@@ -31,10 +31,10 @@
         <location filename="../notifyd.cpp" line="299"/>
         <location filename="../notifyd.cpp" line="319"/>
         <source>%n Unattended Notification(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n непрочитанное уведомление</numerusform>
+            <numerusform>%n непрочитанных уведомления</numerusform>
+            <numerusform>%n непрочитанных уведомлений</numerusform>
         </translation>
     </message>
 </context>
