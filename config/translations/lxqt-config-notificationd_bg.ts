01aa785f8a3361958d3b6082f1e470a5946642c0
Mincho Kondarev <mkondarev@yahoo.de>
2020-12-07 08:29:23 +0100

    Weblate commits
    Co-authored-by: Mincho Kondarev <mkondarev@yahoo.de>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-notificationd/bg/
    Translation: LXQt Configuration/lxqt-config-notificationd
    Translation: LXQt Configuration/lxqt-notificationd

---
+++ config/translations/lxqt-config-notificationd_bg.ts
@@ -0,0 +1,153 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bg">
+<context>
+    <name>AdvancedSettings</name>
+    <message>
+        <location filename="../advancedsettings.ui" line="67"/>
+        <source>Sizes</source>
+        <translation>Размер</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="75"/>
+        <source>Width:</source>
+        <translation>Ширина:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="82"/>
+        <location filename="../advancedsettings.ui" line="106"/>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="99"/>
+        <source>Spacing:</source>
+        <translation>Отстояние:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="118"/>
+        <source>Duration</source>
+        <translation>Продължителност</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="130"/>
+        <source>Some notifications set their own on-screen duration.</source>
+        <translation>Някои известия задават собствена продължителност на показване.</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="142"/>
+        <source>Default duration:</source>
+        <translation>Продължителност по подразбиране:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="149"/>
+        <source> sec</source>
+        <translation> s</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="177"/>
+        <source>Screen</source>
+        <translation>Екран</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="183"/>
+        <source>When unchecked the notification will always show on primary screen</source>
+        <translation>Ако опцията не е избрана известията ще се показват на главния екран</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="186"/>
+        <source>Show notifications on screen with the mouse</source>
+        <translation>Показване на известията на екрана с мишката</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="17"/>
+        <source>Unattended Notifications</source>
+        <translation>Непрочетени съобщения</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="25"/>
+        <source>How many to save:</source>
+        <translation>Брой на запазени съобщения:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="39"/>
+        <location filename="../advancedsettings.ui" line="49"/>
+        <source>Application name is on the top of notification.</source>
+        <translation>Разположение на името на приложението отгоре на известието.</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="42"/>
+        <source>Ignore these applications:</source>
+        <translation>Игнориране на тези приложения:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="52"/>
+        <source>app1,app2,app3</source>
+        <translation>app1,app2,app3</translation>
+    </message>
+</context>
+<context>
+    <name>BasicSettings</name>
+    <message>
+        <location filename="../basicsettings.ui" line="23"/>
+        <source>Basic Settings</source>
+        <translation>Основни настройки</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="30"/>
+        <source>Position on screen</source>
+        <translation>Позиция на екрана</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="61"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; notifications daemon is slow to respond.
+Keep trying to connect…</source>
+        <translation>&lt;b&gt;Предупреждение:&lt;/b&gt; Демонът на известията отговаря забавено.
+Продължаване с опитите за свързване…</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="70"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
+A fallback will be used.</source>
+        <translation>&lt;b&gt;Предупреждение:&lt;/b&gt; Не се изпълнява демон за известия.
+Ще бъде използван резервен процес.</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="73"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
+These settings won&apos;t have any effect on it!</source>
+        <translation>&lt;b&gt;Предупреждение:&lt;/b&gt; Изпълнява се демон за известия на трета страна (%1).
+Ще бъде използван резервен процес!</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="131"/>
+        <source>Notification demo </source>
+        <translation>Демо известие </translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="132"/>
+        <source>This is a test notification.
+ All notifications will now appear here on LXQt.</source>
+        <translation>Това е тестово известие.
+ Всички известия вече ще се показват тук в LXQt.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="41"/>
+        <source>Desktop Notifications</source>
+        <translation>Известия на работния плот</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="44"/>
+        <source>Basic Settings</source>
+        <translation>Основни настройки</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="48"/>
+        <source>Advanced Settings</source>
+        <translation>Разширени настройки</translation>
+    </message>
+</context>
+</TS>
---
+++ src/translations/lxqt-notificationd_bg.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bg">
+<context>
+    <name>NotificationActionsComboWidget</name>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="103"/>
+        <source>Actions:</source>
+        <translation>Действие:</translation>
+    </message>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="122"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+<context>
+    <name>Notifyd</name>
+    <message>
+        <location filename="../notifyd.cpp" line="227"/>
+        <source>Clear All</source>
+        <translation>Изчистване на всички</translation>
+    </message>
+    <message>
+        <location filename="../notifyd.cpp" line="235"/>
+        <source>Options</source>
+        <translation>Опции</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../notifyd.cpp" line="242"/>
+        <location filename="../notifyd.cpp" line="300"/>
+        <location filename="../notifyd.cpp" line="320"/>
+        <source>%n Unattended Notification(s)</source>
+        <translation>
+            <numerusform>%n непрочетено съобщение</numerusform>
+            <numerusform>%n непрочетени съобщения</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
