d014627cd217035842a5993c471bb2924ccbb335
Ihor Hordiichuk <igor_ck@outlook.com>
2021-02-23 13:06:50 +0100

    Weblate commits
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/uk/
    Translation: LXQt Configuration/lxqt-config-notificationd

--- config/translations/lxqt-config-notificationd_uk.ts
+++ config/translations/lxqt-config-notificationd_uk.ts
@@ -17,7 +17,7 @@
         <location filename="../advancedsettings.ui" line="82"/>
         <location filename="../advancedsettings.ui" line="106"/>
         <source> px</source>
-        <translation> пт</translation>
+        <translation> пікселів</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="99"/>
@@ -47,43 +47,43 @@
     <message>
         <location filename="../advancedsettings.ui" line="177"/>
         <source>Screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Екран</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="183"/>
         <source>When unchecked the notification will always show on primary screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Якщо не встановлено прапорець, сповіщення завжди з&apos;являтимуться на основному екрані</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="186"/>
         <source>Show notifications on screen with the mouse</source>
-        <translation type="unfinished"></translation>
+        <translation>Показувати сповіщення на екрані, на якому знаходиться вказівник</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="17"/>
         <source>Unattended Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Непрочитані сповіщення</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="25"/>
         <source>How many to save:</source>
-        <translation type="unfinished"></translation>
+        <translation>Скільки зберігати:</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="39"/>
         <location filename="../advancedsettings.ui" line="49"/>
         <source>Application name is on the top of notification.</source>
-        <translation type="unfinished"></translation>
+        <translation>Назва програми знаходиться вгорі сповіщення.</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="42"/>
         <source>Ignore these applications:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ігнорувати ці програми:</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="52"/>
         <source>app1,app2,app3</source>
-        <translation type="unfinished"></translation>
+        <translation>програма1,програма2,програма3</translation>
     </message>
 </context>
 <context>
@@ -102,7 +102,8 @@
         <location filename="../basicsettings.cpp" line="61"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; notifications daemon is slow to respond.
 Keep trying to connect…</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Попередження:&lt;/b&gt; фонова служба сповіщень повільно відповідає.
+Продовжуємо спроби з&apos;єднання…</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="70"/>
