b1b14a87b11af0de141ebe6c656ba618f8a13f4f
Allan Nordhøy <epost@anotheragency.no>
2021-03-04 06:07:04 +0100

    Weblate commits
    Co-authored-by: Allan Nordhøy <epost@anotheragency.no>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/nb_NO/
    Translation: LXQt Configuration/lxqt-config-notificationd

--- config/translations/lxqt-config-notificationd_nb_NO.ts
+++ config/translations/lxqt-config-notificationd_nb_NO.ts
@@ -47,17 +47,17 @@
     <message>
         <location filename="../advancedsettings.ui" line="177"/>
         <source>Screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Skjerm</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="183"/>
         <source>When unchecked the notification will always show on primary screen</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Når fravalgt vil merknaden alltid vises på hovedskjermen</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="186"/>
         <source>Show notifications on screen with the mouse</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Vis merknader på skjermen pekeren er på</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="17"/>
@@ -102,7 +102,8 @@
         <location filename="../basicsettings.cpp" line="61"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; notifications daemon is slow to respond.
 Keep trying to connect…</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&lt;b&gt;Advarsel:&lt;/b&gt; merknadsnissen lar vente på seg med å svare.
+Prøver stadig å koble til …</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="70"/>
