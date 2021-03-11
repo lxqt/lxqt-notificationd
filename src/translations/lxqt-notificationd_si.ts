6a0c0aafb5f60052b0325df21668aa4c2f8e6704
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-03-11 18:52:56 +0100

    Translations update from Weblate (#250)
    * Weblate commits
    
    Co-authored-by: helabasa <R45XvezA@pm.me>
    
    * Weblate commits
    
    Co-authored-by: Allan Nordhøy <epost@anotheragency.no>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-notificationd/nb_NO/
    Translation: LXQt Configuration/lxqt-notificationd
    
    * Weblate commits
    
    Co-authored-by: Standreas <standreas@riseup.net>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd-menu-entry/it/
    Translation: LXQt Configuration/lxqt-config-notificationd Menu Entry
    
    Co-authored-by: helabasa <R45XvezA@pm.me>
    Co-authored-by: Allan Nordhøy <epost@anotheragency.no>
    Co-authored-by: Standreas <standreas@riseup.net>

--- src/translations/lxqt-notificationd_si.ts
+++ src/translations/lxqt-notificationd_si.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="nb_NO">
+<TS version="2.1" language="si">
 <context>
     <name>NotificationActionsComboWidget</name>
     <message>
         <location filename="../notificationwidgets.cpp" line="103"/>
         <source>Actions:</source>
-        <translation>Handlinger:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../notificationwidgets.cpp" line="122"/>
         <source>OK</source>
-        <translation>Okei</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -19,21 +19,21 @@
     <message>
         <location filename="../notifyd.cpp" line="227"/>
         <source>Clear All</source>
-        <translation>Fjern alt</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../notifyd.cpp" line="235"/>
         <source>Options</source>
-        <translation>Valg</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message numerus="yes">
         <location filename="../notifyd.cpp" line="242"/>
         <location filename="../notifyd.cpp" line="300"/>
         <location filename="../notifyd.cpp" line="320"/>
         <source>%n Unattended Notification(s)</source>
-        <translation>
-            <numerusform>%n usett varsel</numerusform>
-            <numerusform>%n usette varsler</numerusform>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
         </translation>
     </message>
 </context>
