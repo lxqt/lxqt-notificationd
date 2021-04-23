19fa688c17d96c63a51a672456b19dc02e064bfb
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-04-23 15:34:21 +0200

    Weblate commits (#261)
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-notificationd/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/notification-daemon-module-name/et/
    Translation: LXQt Configuration/Notification Daemon Module Name
    Translation: LXQt Configuration/lxqt-notificationd
    
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>

---
+++ src/translations/lxqt-notificationd_et.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>NotificationActionsComboWidget</name>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="103"/>
+        <source>Actions:</source>
+        <translation>Tegevused:</translation>
+    </message>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="122"/>
+        <source>OK</source>
+        <translation>Sobib</translation>
+    </message>
+</context>
+<context>
+    <name>Notifyd</name>
+    <message>
+        <location filename="../notifyd.cpp" line="227"/>
+        <source>Clear All</source>
+        <translation>Kustuta kõik</translation>
+    </message>
+    <message>
+        <location filename="../notifyd.cpp" line="235"/>
+        <source>Options</source>
+        <translation>Valikud</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../notifyd.cpp" line="242"/>
+        <location filename="../notifyd.cpp" line="300"/>
+        <location filename="../notifyd.cpp" line="320"/>
+        <source>%n Unattended Notification(s)</source>
+        <translation>
+            <numerusform>%n lugemata teavitus</numerusform>
+            <numerusform>%n lugemata teavitust</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
