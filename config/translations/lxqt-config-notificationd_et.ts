688c1119a747820e43f61fbc11f09e82c42a586c
Priit Jõerüüt <lxqtwlate@joeruut.com>
2021-05-01 20:07:16 +0200

    Weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/et/
    Translation: LXQt Configuration/lxqt-config-notificationd

---
+++ config/translations/lxqt-config-notificationd_et.ts
@@ -0,0 +1,153 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>AdvancedSettings</name>
+    <message>
+        <location filename="../advancedsettings.ui" line="67"/>
+        <source>Sizes</source>
+        <translation>Suurused</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="75"/>
+        <source>Width:</source>
+        <translation>Laius:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="82"/>
+        <location filename="../advancedsettings.ui" line="106"/>
+        <source> px</source>
+        <translation> pikslit</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="99"/>
+        <source>Spacing:</source>
+        <translation>Ruum teavituste vahel:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="118"/>
+        <source>Duration</source>
+        <translation>Kuvamise kestus</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="130"/>
+        <source>Some notifications set their own on-screen duration.</source>
+        <translation>Mõned teavitused kontrollivad ise oma kuvamise kestust.</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="142"/>
+        <source>Default duration:</source>
+        <translation>Vaikimisi kestus:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="149"/>
+        <source> sec</source>
+        <translation> sekund(it)</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="177"/>
+        <source>Screen</source>
+        <translation>Ekraan</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="183"/>
+        <source>When unchecked the notification will always show on primary screen</source>
+        <translation>Kui see eelistus pole valitud, siis kuvame teavitusi alati esmasel ekraanil</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="186"/>
+        <source>Show notifications on screen with the mouse</source>
+        <translation>Näita teavitusi ekraanil, kus asub hiirekursor</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="17"/>
+        <source>Unattended Notifications</source>
+        <translation>Lugemata teavitused</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="25"/>
+        <source>How many to save:</source>
+        <translation>Kui mitut salvestame:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="39"/>
+        <location filename="../advancedsettings.ui" line="49"/>
+        <source>Application name is on the top of notification.</source>
+        <translation>Rakenduse nimi on teavituse esimeses reas.</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="42"/>
+        <source>Ignore these applications:</source>
+        <translation>Eira neid rakendusi:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="52"/>
+        <source>app1,app2,app3</source>
+        <translation>rakendus1,rakendus2,rakendus3</translation>
+    </message>
+</context>
+<context>
+    <name>BasicSettings</name>
+    <message>
+        <location filename="../basicsettings.ui" line="23"/>
+        <source>Basic Settings</source>
+        <translation>Põhiseadistused</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="30"/>
+        <source>Position on screen</source>
+        <translation>Kuvamise asukoht ekraanil</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="61"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; notifications daemon is slow to respond.
+Keep trying to connect…</source>
+        <translation>&lt;b&gt;Hoiatus:&lt;/b&gt; teavituste taustarakendus reageerib hetkel aeglaselt.
+Palun jätka ühenduse katseid…</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="70"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
+A fallback will be used.</source>
+        <translation>&lt;b&gt;Hoiatus:&lt;/b&gt; teavituste taustarakendus ei tööta.
+Kasutame alternatiivset lahendust.</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="73"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
+These settings won&apos;t have any effect on it!</source>
+        <translation>&lt;b&gt;Warning:&lt;/b&gt; Töötab üks teine teavituste taustateenus (%1).
+Need seadistused seda teenust ei mõjuta!</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="131"/>
+        <source>Notification demo </source>
+        <translation>Teavituste demo </translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="132"/>
+        <source>This is a test notification.
+ All notifications will now appear here on LXQt.</source>
+        <translation>See on teavistuse näidis.
+ LXQt teavitused saavad olema nüüd sellised.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="41"/>
+        <source>Desktop Notifications</source>
+        <translation>Töölaua teavitused</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="44"/>
+        <source>Basic Settings</source>
+        <translation>Põhiseadistused</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="48"/>
+        <source>Advanced Settings</source>
+        <translation>Lisaseadistused</translation>
+    </message>
+</context>
+</TS>
