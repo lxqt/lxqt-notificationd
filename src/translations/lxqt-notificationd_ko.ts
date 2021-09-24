1f11b50a307c903a68ea9b3c5f02377c9389ed86
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-09-25 00:05:42 +0800

    Weblate commits (#277)
    Co-authored-by: 이정희 <daemul72@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-notificationd/ko/
    Translation: LXQt Configuration/lxqt-notificationd

---
+++ src/translations/lxqt-notificationd_ko.ts
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko">
+<context>
+    <name>NotificationActionsComboWidget</name>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="103"/>
+        <source>Actions:</source>
+        <translation>동작:</translation>
+    </message>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="122"/>
+        <source>OK</source>
+        <translation>확인</translation>
+    </message>
+</context>
+<context>
+    <name>Notifyd</name>
+    <message>
+        <location filename="../notifyd.cpp" line="227"/>
+        <source>Clear All</source>
+        <translation>모두 지우기</translation>
+    </message>
+    <message>
+        <location filename="../notifyd.cpp" line="235"/>
+        <source>Options</source>
+        <translation>옵션</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../notifyd.cpp" line="242"/>
+        <location filename="../notifyd.cpp" line="300"/>
+        <location filename="../notifyd.cpp" line="320"/>
+        <source>%n Unattended Notification(s)</source>
+        <translation>
+            <numerusform>%n개 방치된 알림</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
