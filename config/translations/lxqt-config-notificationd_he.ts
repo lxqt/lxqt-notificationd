86087c304b8f6db5c2cb4f2e50513de1d26fbf07
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-10-15 09:05:05 +0200

    Weblate commits (#281)
    Co-authored-by: Dwi Kristianto <steamboatid@gmail.com>
    Co-authored-by: Paulo Santiago <falecom.pw@gmail.com>
    Co-authored-by: Yaron Shahrabani <sh.yaron@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/he/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/pt_BR/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-notificationd/id/
    Translation: LXQt Configuration/lxqt-config-notificationd
    Translation: LXQt Configuration/lxqt-notificationd

--- config/translations/lxqt-config-notificationd_he.ts
+++ config/translations/lxqt-config-notificationd_he.ts
@@ -62,12 +62,12 @@
     <message>
         <location filename="../advancedsettings.ui" line="196"/>
         <source>Do Not Disturb</source>
-        <translation type="unfinished"></translation>
+        <translation>לא להפריע</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="202"/>
         <source>Only save notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>לשמור התראות בלבד</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="17"/>
