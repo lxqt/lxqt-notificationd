5b301628e810a0e67d9787c8270e5724a5b108c3
Weblate <noreply@weblate.org>
2021-11-09 10:08:24 +0100

    Weblate commits
    Co-authored-by: Dimitrios Glentadakis <dglent@free.fr>
    Co-authored-by: Dr. Ayhan YALÇINSOY <ayhanyalcinsoy@pisilinux.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/el/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/tr/
    Translation: LXQt Configuration/lxqt-config-notificationd

--- config/translations/lxqt-config-notificationd_tr.ts
+++ config/translations/lxqt-config-notificationd_tr.ts
@@ -62,12 +62,12 @@
     <message>
         <location filename="../advancedsettings.ui" line="196"/>
         <source>Do Not Disturb</source>
-        <translation type="unfinished"></translation>
+        <translation>Rahatsız Etme</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="202"/>
         <source>Only save notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Yalnızca bildirimleri kaydet</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="17"/>
