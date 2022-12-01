d5504be235b2acc8cc2cc64f52350d283876ee76
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2022-12-01 23:20:29 +0800

    Weblate commits (#334)
    Co-authored-by: Baurzhan Muftakhidinov <baurthefirst@gmail.com>
    Co-authored-by: Carl Andersen <carl@flying-cloud.dk>
    Co-authored-by: Yaron Shahrabani <sh.yaron@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd-menu-entry/kk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-notificationd/he/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-notificationd/kk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/notification-daemon-module-name/kk/
    Translation: LXQt Configuration/Notification Daemon Module Name
    Translation: LXQt Configuration/lxqt-config-notificationd
    Translation: LXQt Configuration/lxqt-config-notificationd Menu Entry
    Translation: LXQt Configuration/lxqt-notificationd

--- src/translations/lxqt-notificationd_kk.ts
+++ src/translations/lxqt-notificationd_kk.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="he">
+<TS version="2.1" language="kk">
 <context>
     <name>NotificationActionsComboWidget</name>
     <message>
         <location filename="../notificationwidgets.cpp" line="131"/>
         <source>Actions:</source>
-        <translation>פעולות:</translation>
+        <translation>Әрекеттер:</translation>
     </message>
     <message>
         <location filename="../notificationwidgets.cpp" line="155"/>
         <source>OK</source>
-        <translation>אישור</translation>
+        <translation>ОК</translation>
     </message>
 </context>
 <context>
@@ -19,12 +19,12 @@
     <message>
         <location filename="../notifyd.cpp" line="268"/>
         <source>Clear All</source>
-        <translation>לפנות הכול</translation>
+        <translation>Барлығын тазарту</translation>
     </message>
     <message>
         <location filename="../notifyd.cpp" line="276"/>
         <source>Options</source>
-        <translation>אפשרויות</translation>
+        <translation>Баптаулар</translation>
     </message>
     <message numerus="yes">
         <location filename="../notifyd.cpp" line="283"/>
@@ -32,10 +32,8 @@
         <location filename="../notifyd.cpp" line="360"/>
         <source>%n Unattended Notification(s)</source>
         <translation>
-            <numerusform>התראה %n שלא התייחסת אליה</numerusform>
-            <numerusform>%n התראות שלא התייחסת אליהן</numerusform>
-            <numerusform>%n התראות שלא התייחסת אליהן</numerusform>
-            <numerusform>%n התראות שלא התייחסת אליהן</numerusform>
+            <numerusform>%n оқылмаған хабарлама</numerusform>
+            <numerusform>%n оқылмаған хабарлама</numerusform>
         </translation>
     </message>
 </context>
