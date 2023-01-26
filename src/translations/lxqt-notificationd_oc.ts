60e9d4be1f02b7574662537c66fae01e1152884f
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2023-01-27 03:04:16 +0800

    Weblate commits (#335)
    Co-authored-by: Quentin PAGÈS <quentinantonin@free.fr>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd-menu-entry/oc/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-notificationd/oc/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/notification-daemon-module-name/oc/
    Translation: LXQt Configuration/Notification Daemon Module Name
    Translation: LXQt Configuration/lxqt-config-notificationd Menu Entry
    Translation: LXQt Configuration/lxqt-notificationd

---
+++ src/translations/lxqt-notificationd_oc.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="oc">
+<context>
+    <name>NotificationActionsComboWidget</name>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="131"/>
+        <source>Actions:</source>
+        <translation>Accions :</translation>
+    </message>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="155"/>
+        <source>OK</source>
+        <translation>D’acòrdi</translation>
+    </message>
+</context>
+<context>
+    <name>Notifyd</name>
+    <message>
+        <location filename="../notifyd.cpp" line="268"/>
+        <source>Clear All</source>
+        <translation>Tot escafar</translation>
+    </message>
+    <message>
+        <location filename="../notifyd.cpp" line="276"/>
+        <source>Options</source>
+        <translation>Opcions</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../notifyd.cpp" line="283"/>
+        <location filename="../notifyd.cpp" line="340"/>
+        <location filename="../notifyd.cpp" line="360"/>
+        <source>%n Unattended Notification(s)</source>
+        <translation>
+            <numerusform>%n notificacion pas legida</numerusform>
+            <numerusform>%n notificacions pas legidas</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
