67fd468ab6ddabdccaf1329937bef8b15aa7883e
Weblate <noreply@weblate.org>
2023-05-25 00:11:46 +0200

    Weblate commits
    Co-authored-by: Jouni Järvinen <jounijarvis@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-notificationd/fi/
    Translation: LXQt Configuration/lxqt-config-notificationd
    Translation: LXQt Configuration/lxqt-notificationd

---
+++ config/translations/lxqt-config-notificationd_fi.ts
@@ -0,0 +1,203 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fi">
+<context>
+    <name>AdvancedSettings</name>
+    <message>
+        <location filename="../advancedsettings.ui" line="67"/>
+        <source>Sizes</source>
+        <translation>Koot</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="75"/>
+        <source>Width:</source>
+        <translation>Leveys:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="82"/>
+        <location filename="../advancedsettings.ui" line="106"/>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="99"/>
+        <source>Spacing:</source>
+        <translation>Välit:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="118"/>
+        <source>Duration</source>
+        <translation>Kesto</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="130"/>
+        <source>Some notifications set their own on-screen duration.</source>
+        <translation>Joillakin ilmoituksilla on oma näkyvyysaikansa.</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="142"/>
+        <source>Default duration:</source>
+        <translation>Oletuskesto:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="149"/>
+        <source> sec</source>
+        <translation> sek</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="177"/>
+        <source>Screen</source>
+        <translation>Näyttö</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="183"/>
+        <source>When unchecked the notification will always show on primary screen</source>
+        <translation>Jos ei valittu, ilmoitus näytetään aina ensisijaisella näytöllä</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="186"/>
+        <source>Show notifications on screen with the mouse</source>
+        <translation>Näytä ilmoitukset samalla näytöllä kuin hiiri</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="196"/>
+        <source>Do Not Disturb</source>
+        <translation>Älä häiritse</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="202"/>
+        <source>Only save notifications</source>
+        <translation>Pelkästään tallenna ilmoitukset</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="17"/>
+        <source>Unattended Notifications</source>
+        <translation>Valvomattomat ilmoitukset</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="25"/>
+        <source>How many to save:</source>
+        <translation>Tallennettavien määrä:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="39"/>
+        <location filename="../advancedsettings.ui" line="49"/>
+        <source>Application name is on the top of notification.</source>
+        <translation>Sovelluksen nimi näytetään ilmoituksen yläpäässä.</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="42"/>
+        <source>Ignore these applications:</source>
+        <translation>Ohita nämä sovellukset:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="52"/>
+        <source>app1,app2,app3</source>
+        <translation>sov1,sov2,sov3</translation>
+    </message>
+</context>
+<context>
+    <name>BasicSettings</name>
+    <message>
+        <location filename="../basicsettings.ui" line="17"/>
+        <source>Position on screen</source>
+        <translation>Sijainti näytöllä</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="176"/>
+        <source>Preview</source>
+        <translation>Esikatselu</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="63"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; notifications daemon is slow to respond.
+Keep trying to connect…</source>
+        <translation>&lt;b&gt;Varoitus:&lt;/b&gt; ilmoituspalvelin vastaa hitaasti.
+Yritetään yhdistää uudelleen…</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="72"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
+A fallback will be used.</source>
+        <translation>&lt;b&gt;Varoitus:&lt;/b&gt; Ilmoituspalvelin ei ole päällä.
+Käytetään varajärjestelmää.</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="75"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
+These settings won&apos;t have any effect on it!</source>
+        <translation>&lt;b&gt;Varoitus:&lt;/b&gt; Näillä asetuksilla ei ole vaikutusta 
+kolmannen osapuolen ilmoituspalvelimiin (%1)!</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="137"/>
+        <source>at top left</source>
+        <translation>vasemmassa yläkulmassa</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="139"/>
+        <source>at top center</source>
+        <translation>ylhäällä keskellä</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="141"/>
+        <source>at top right</source>
+        <translation>oikeassa ylänurkassa</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="143"/>
+        <source>at center left</source>
+        <translation>vasemmalla keskellä</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="145"/>
+        <source>at center right</source>
+        <translation>oikealla keskellä</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="147"/>
+        <source>at bottom left</source>
+        <translation>vasemmassa alanurkassa</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="149"/>
+        <source>at bottom center</source>
+        <translation>alhaalla keskellä</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="151"/>
+        <source>at bottom right</source>
+        <translation>oikeassa alanurkassa</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="153"/>
+        <source>Notification demo </source>
+        <translation>Testi-ilmoitus </translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="154"/>
+        <source>This is a test notification.
+ All notifications will now appear here on LXQt.</source>
+        <translation>Tämä on testi-ilmoitus.
+ LXQt näyttää kaikki ilmoitukset täällä.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="41"/>
+        <source>Desktop Notifications</source>
+        <translation>Työpöydän ilmoitukset</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="44"/>
+        <source>General Settings</source>
+        <translation>Yleisasetukset</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="48"/>
+        <source>Position</source>
+        <translation>Sijainti</translation>
+    </message>
+</context>
+</TS>
---
+++ src/translations/lxqt-notificationd_fi.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fi">
+<context>
+    <name>NotificationActionsComboWidget</name>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="131"/>
+        <source>Actions:</source>
+        <translation>Toiminnot:</translation>
+    </message>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="155"/>
+        <source>OK</source>
+        <translation>Ok</translation>
+    </message>
+</context>
+<context>
+    <name>Notifyd</name>
+    <message>
+        <location filename="../notifyd.cpp" line="268"/>
+        <source>Clear All</source>
+        <translation>Tyhjennä luettelo</translation>
+    </message>
+    <message>
+        <location filename="../notifyd.cpp" line="276"/>
+        <source>Options</source>
+        <translation>Asetukset</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../notifyd.cpp" line="283"/>
+        <location filename="../notifyd.cpp" line="340"/>
+        <location filename="../notifyd.cpp" line="360"/>
+        <source>%n Unattended Notification(s)</source>
+        <translation>
+            <numerusform>%n valvomaton ilmoitus</numerusform>
+            <numerusform>%n valvomattomia ilmoituksia</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
