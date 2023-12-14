2e81a3962d6d47481cb2cda94a6d58ce717ecf7d
Weblate <noreply@weblate.org>
2023-12-14 20:59:29 +0100

    Weblate commits
    Co-authored-by: Andrei Stepanov <adem4ik@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: Xavier Paniello <xavip@medusaweb.cat>
    Co-authored-by: cybercop <cybercop_montana@abv.bg>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/ca/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/ru/
    Translation: LXQt Configuration/lxqt-config-notificationd

--- config/translations/lxqt-config-notificationd_ca.ts
+++ config/translations/lxqt-config-notificationd_ca.ts
@@ -197,7 +197,7 @@ Els ajusts no prendran cap efecte!</translation>
     <message>
         <location filename="../mainwindow.cpp" line="48"/>
         <source>Appearance</source>
-        <translation type="unfinished"></translation>
+        <translation>Aparença</translation>
     </message>
 </context>
 </TS>
