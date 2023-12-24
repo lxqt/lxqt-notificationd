6e7f30da310d3b4668107d1382d7ac0f570fe0e7
Weblate <noreply@weblate.org>
2023-12-24 21:23:02 +0100

    Weblate commits
    Co-authored-by: A S Alam <amanpreet.alam@gmail.com>
    Co-authored-by: Roberalz <lumintnian@outlook.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd-menu-entry/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/gl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-notificationd/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/notification-daemon-module-name/pa/
    Translation: LXQt Configuration/Notification Daemon Module Name
    Translation: LXQt Configuration/lxqt-config-notificationd
    Translation: LXQt Configuration/lxqt-config-notificationd Menu Entry
    Translation: LXQt Configuration/lxqt-notificationd

--- config/translations/lxqt-config-notificationd_pa.ts
+++ config/translations/lxqt-config-notificationd_pa.ts
@@ -1,78 +1,78 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="gl">
+<TS version="2.1" language="pa">
 <context>
     <name>AdvancedSettings</name>
     <message>
         <location filename="../advancedsettings.ui" line="67"/>
         <source>Duration</source>
-        <translation>Duración</translation>
+        <translation>ਅੰਤਰਾਲ</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="79"/>
         <source>Some notifications set their own on-screen duration.</source>
-        <translation>Algunhas notificacións estabelecen a súa propia duración en pantalla.</translation>
+        <translation>ਕੁਝ ਨੋਟੀਫਿਕੇਸ਼ਨ ਆਪਣਾ ਆਨ-ਸਕਰੀਨ ਸਮਾਂ ਖੁਦ ਸੈੱਟ ਕਰਦੇ ਹਨ।</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="91"/>
         <source>Default duration:</source>
-        <translation>Duración predeterminada:</translation>
+        <translation>ਮੂਲ ਅੰਤਰਾਲ:</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="98"/>
         <source> sec</source>
-        <translation> seg</translation>
+        <translation> ਸਕਿੰਟ</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="126"/>
         <source>Screen</source>
-        <translation>Pantalla</translation>
+        <translation>ਸਕਰੀਨ</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="132"/>
         <source>When unchecked the notification will always show on primary screen</source>
-        <translation>Cando se desmarca a notificación sempre aparecerá na pantalla principal</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="135"/>
         <source>Show notifications on screen with the mouse</source>
-        <translation>Mostra as notificacións na pantalla co rato</translation>
+        <translation>ਮਾਊਸ ਵਾਲੀ ਸਕਰੀਨ ਉੱਤੇ ਨੋਟੀਫਿਕੇਸ਼ਨ ਵੇਖਾਓ</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="145"/>
         <source>Do Not Disturb</source>
-        <translation>Non molestar</translation>
+        <translation>ਤੰਗ ਨਾ ਕਰੋ</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="151"/>
         <source>Only save notifications</source>
-        <translation>Garda só as notificacións</translation>
+        <translation>ਨੋਟੀਫਿਕੇਸ਼ਨਾਂ ਨੂੰ ਸਿਰਫ਼ ਸੰਭਾਲੋ</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="17"/>
         <source>Unattended Notifications</source>
-        <translation>Notificacións desatendidas</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="25"/>
         <source>How many to save:</source>
-        <translation>Cantos hai para gardar:</translation>
+        <translation>ਕਿੰਨੇ ਸੰਭਾਲਣੇ ਹਨ:</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="39"/>
         <location filename="../advancedsettings.ui" line="49"/>
         <source>Application name is on the top of notification.</source>
-        <translation>O nome da aplicación atópase na parte superior da notificación.</translation>
+        <translation>ਨੋਟੀਫਿਕੇਸ਼ਨ ਦੇ ਸਿਖਰ ਉੱਤੇ ਐਪਲੀਕੇਸ਼ਨ ਦਾ ਨਾਂ।</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="42"/>
         <source>Ignore these applications:</source>
-        <translation>Ignorar estas aplicacións:</translation>
+        <translation>ਇਹ ਐਪਲੀਕੇਸ਼ਨਾਂ ਨੂੰ ਅਣਡਿੱਠਾ ਕਰੋ:</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="52"/>
         <source>app1,app2,app3</source>
-        <translation>apli1,apli2,apli3</translation>
+        <translation>ਐਪ1,ਐਪ2,ਐਪ3</translation>
     </message>
 </context>
 <context>
@@ -80,106 +80,103 @@
     <message>
         <location filename="../basicsettings.ui" line="17"/>
         <source>Sizes</source>
-        <translation>Medidas</translation>
+        <translation>ਆਕਾਰ</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="25"/>
         <source>Width:</source>
-        <translation>Largo:</translation>
+        <translation>ਚੌੜਾਈ:</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="32"/>
         <location filename="../basicsettings.ui" line="56"/>
         <source> px</source>
-        <translation> px</translation>
+        <translation> ਪਿਕ</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="49"/>
         <source>Spacing:</source>
-        <translation>Espazado:</translation>
+        <translation>ਫਾਸਲਾ:</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="68"/>
         <source>Position on screen</source>
-        <translation>Posición na pantalla</translation>
+        <translation>ਸਕਰੀਨ ਉੱਤੇ ਸਥਿਤੀ</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="227"/>
         <source>Preview</source>
-        <translation>Vista previa</translation>
+        <translation>ਝਲਕ</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="65"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; notifications daemon is slow to respond.
 Keep trying to connect…</source>
-        <translation>&lt;b&gt;Alerta:&lt;/b&gt; o demonip de notificacións tarda en responder.
-Sigue tentando conectarse…</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="74"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
 A fallback will be used.</source>
-        <translation>&lt;b&gt;Aviso:&lt;/b&gt; Non hai ningún servizo de notificación en execución.
-Empregarase un método alternativo.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="77"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
 These settings won&apos;t have any effect on it!</source>
-        <translation>&lt;b&gt;Aviso:&lt;/b&gt; Hai un servizo de notificacións de terceiros (%1) en execución.
-Estes axustes non lle afectarán!</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="142"/>
         <source>at top left</source>
-        <translation>na parte superior esquerda</translation>
+        <translation>ਉੱਤੇ ਖੱਬੇ</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="144"/>
         <source>at top center</source>
-        <translation>no centro superior</translation>
+        <translation>ਉੱਤੇ ਸੈਂਟਰ</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="146"/>
         <source>at top right</source>
-        <translation>na parte superior dereita</translation>
+        <translation>ਉੱਤੇ ਸੱਜੇ</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="148"/>
         <source>at center left</source>
-        <translation>no centro esquerdo</translation>
+        <translation>ਸੈਂਟਰ ਖੱਬੇ</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="150"/>
         <source>at center right</source>
-        <translation>no centro dereito</translation>
+        <translation>ਸਸੈੰਟਰ ਸੱਜੇ</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="152"/>
         <source>at bottom left</source>
-        <translation>na parte inferior esquerda</translation>
+        <translation>ਹੇਠਾਂ ਖੱਬੇ</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="154"/>
         <source>at bottom center</source>
-        <translation>no centro inferior</translation>
+        <translation>ਹੇਠਾਂ ਸੈਂਟਰ</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="156"/>
         <source>at bottom right</source>
-        <translation>na parte inferior dereita</translation>
+        <translation>ਹੇਠਾਂ ਸੱਜੇ</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="158"/>
         <source>Notification demo </source>
-        <translation>Proba de notificación </translation>
+        <translation>ਨੋਟੀਫਿਕੇਸ਼ਨ ਡੈਮੋ </translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="159"/>
         <source>This is a test notification.
  All notifications will now appear here on LXQt.</source>
-        <translation>Isto é unha notificación de proba.
- A partires de agora, todas as notificacións aparecerán aquí en LXQt.</translation>
+        <translation>ਇਠ ਟੈਸਟ ਨੋਟੀਫਿਕੇਸ਼ਨ ਹੈ।
+LXQT ਉੱਤੇ ਸਾਰੇ ਨੋਟੀਫਿਕੇਸ਼ਨ ਇੱਥੇ ਦਿਖਾਈ ਦੇਣਗੇ।</translation>
     </message>
 </context>
 <context>
@@ -187,17 +184,17 @@ Estes axustes non lle afectarán!</translation>
     <message>
         <location filename="../mainwindow.cpp" line="41"/>
         <source>Desktop Notifications</source>
-        <translation>Notificacións do escritorio</translation>
+        <translation>ਡੈਸਕਟਾਪ ਨੋਟੀਫਿਕੇਸ਼ਨ</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="44"/>
         <source>General Settings</source>
-        <translation>Configuración xeral</translation>
+        <translation>ਆਮ ਸੈਟਿੰਗਾਂ</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="48"/>
         <source>Appearance</source>
-        <translation>Aparencia</translation>
+        <translation>ਦਿੱਖ</translation>
     </message>
 </context>
 </TS>
---
+++ src/translations/lxqt-notificationd_pa.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pa">
+<context>
+    <name>NotificationActionsComboWidget</name>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="131"/>
+        <source>Actions:</source>
+        <translation>ਕਾਰਵਾਈਾਂ:</translation>
+    </message>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="155"/>
+        <source>OK</source>
+        <translation>ਠੀਕ ਹੈ</translation>
+    </message>
+</context>
+<context>
+    <name>Notifyd</name>
+    <message>
+        <location filename="../notifyd.cpp" line="269"/>
+        <source>Clear All</source>
+        <translation>ਸਭ ਮਿਟਾਓ</translation>
+    </message>
+    <message>
+        <location filename="../notifyd.cpp" line="277"/>
+        <source>Options</source>
+        <translation>ਚੋਣਾਂ</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../notifyd.cpp" line="284"/>
+        <location filename="../notifyd.cpp" line="341"/>
+        <location filename="../notifyd.cpp" line="361"/>
+        <source>%n Unattended Notification(s)</source>
+        <translation>
+            <numerusform>%n ਬਿਨਾਂ-ਦੇਖੇ ਨੋਟੀਫਿਕੇਸ਼ਨ</numerusform>
+            <numerusform>%n ਬਿਨਾਂ-ਦੇਖੇ ਨੋਟੀਫਿਕੇਸ਼ਨ</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
