1fe42131102a542c3fe8ed96cdf5c88488ad2f49
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2024-02-11 08:11:48 +0100

    Weblate commits (#368)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd-menu-entry/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/lg/
    Translation: LXQt Configuration/lxqt-config-notificationd
    Translation: LXQt Configuration/lxqt-config-notificationd Menu Entry
    
    Co-authored-by: Kizito Birabwa <kbirabwa@yahoo.co.uk>

---
+++ config/translations/lxqt-config-notificationd_lg.ts
@@ -0,0 +1,205 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lg">
+<context>
+    <name>AdvancedSettings</name>
+    <message>
+        <location filename="../advancedsettings.ui" line="67"/>
+        <source>Duration</source>
+        <translation>Obudde obubaka bwe bulabikira</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="79"/>
+        <source>Some notifications set their own on-screen duration.</source>
+        <translation>Obubaka obumu bwesalirawo ebbanga lye bulabikira.</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="91"/>
+        <source>Default duration:</source>
+        <translation>Obudde obwa bulijjo:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="98"/>
+        <source> sec</source>
+        <translation> sik</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="126"/>
+        <source>Screen</source>
+        <translation>Lutimbe</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="132"/>
+        <source>When unchecked the notification will always show on primary screen</source>
+        <translation>Bw&apos;otonyeza wano obubaka bulabikira ku lutimbe akasonga kwe kali</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="135"/>
+        <source>Show notifications on screen with the mouse</source>
+        <translation>Obubaka bulabikire ku lutimbe okuli akasonga</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="145"/>
+        <source>Do Not Disturb</source>
+        <translation>Wewale ebitaataganya</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="151"/>
+        <source>Only save notifications</source>
+        <translation>Obubaka buleme okwolesebwanga,
+buterekebwengabuteresi</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="17"/>
+        <source>Unattended Notifications</source>
+        <translation>Obubaka obutakebedwa</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="25"/>
+        <source>How many to save:</source>
+        <translation>Terekanga bwa mirundi:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="39"/>
+        <location filename="../advancedsettings.ui" line="49"/>
+        <source>Application name is on the top of notification.</source>
+        <translation>Erinnya lya puloguramu eyawerezza obubaka likulemberenga.</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="42"/>
+        <source>Ignore these applications:</source>
+        <translation>Obubaka obuva mu puloguramu
+zino tebuterekebwenga:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="52"/>
+        <source>app1,app2,app3</source>
+        <translation>pulog1,pulog2,pulog3</translation>
+    </message>
+</context>
+<context>
+    <name>BasicSettings</name>
+    <message>
+        <location filename="../basicsettings.ui" line="17"/>
+        <source>Sizes</source>
+        <translation>Bunene bw&apos;akadirisa ak&apos;obubaka</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="25"/>
+        <source>Width:</source>
+        <translation>Bugazi:</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="32"/>
+        <location filename="../basicsettings.ui" line="56"/>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="49"/>
+        <source>Spacing:</source>
+        <translation>Ebbanga eryawula obudirisa:</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="68"/>
+        <source>Position on screen</source>
+        <translation>Webulabikira ku lutimbe</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="227"/>
+        <source>Preview</source>
+        <translation>Kyegezemu</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="65"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; notifications daemon is slow to respond.
+Keep trying to connect…</source>
+        <translation>&lt;b&gt;Kulabula&lt;/b&gt; dayimoni ekola ku bubaka ekola mpola.
+Yongera okugezako…</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="74"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
+A fallback will be used.</source>
+        <translation>&lt;b&gt;Kulabula:&lt;/b&gt; Dayimoni ekola ku bubaka tetandikidwa.
+Sisitemu egenda okugeza okukozesamu ekirala.</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="77"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
+These settings won&apos;t have any effect on it!</source>
+        <translation>,b.Kulabula&lt;/b&gt; Dayimoni ekola ku bubaka yakolebwa balala (%1).
+Ebitegekedwa wano tebigenda okugikolako!</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="142"/>
+        <source>at top left</source>
+        <translation>waggulu mu nsonda ya kkono</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="144"/>
+        <source>at top center</source>
+        <translation>waggulu wakati</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="146"/>
+        <source>at top right</source>
+        <translation>waggulu mu nsoda ya ddyo</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="148"/>
+        <source>at center left</source>
+        <translation>ku kkono mu mukkati</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="150"/>
+        <source>at center right</source>
+        <translation>ku ddyo mu makkati</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="152"/>
+        <source>at bottom left</source>
+        <translation>wansi mu nsoda ya kkono</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="154"/>
+        <source>at bottom center</source>
+        <translation>wansi mu makkati</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="156"/>
+        <source>at bottom right</source>
+        <translation>wansi mu nsonda ya ddyo</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="158"/>
+        <source>Notification demo </source>
+        <translation>Kulagako enfaanana ya bubaka </translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="159"/>
+        <source>This is a test notification.
+ All notifications will now appear here on LXQt.</source>
+        <translation>Buno bubaka bwa kugezesabugezesa.
+Obubaka obwa ddala wano we bujja okulabikiranga mu LXQt.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="41"/>
+        <source>Desktop Notifications</source>
+        <translation>Obubaka obulabikira awakolerwa</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="44"/>
+        <source>General Settings</source>
+        <translation>Enteekateeka ez&apos;awamu</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="48"/>
+        <source>Appearance</source>
+        <translation>Endabika</translation>
+    </message>
+</context>
+</TS>
