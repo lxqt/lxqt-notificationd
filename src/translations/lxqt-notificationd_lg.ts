db237e9c1f14971b16c1604638f919abbe29ee31
Weblate <noreply@weblate.org>
2024-02-15 00:06:02 +0100

    Weblate commits
    Co-authored-by: Kizito Birabwa <kbirabwa@yahoo.co.uk>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-notificationd/lg/
    Translation: LXQt Configuration/lxqt-notificationd

---
+++ src/translations/lxqt-notificationd_lg.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lg">
+<context>
+    <name>NotificationActionsComboWidget</name>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="131"/>
+        <source>Actions:</source>
+        <translation>Ebikolwa:</translation>
+    </message>
+    <message>
+        <location filename="../notificationwidgets.cpp" line="155"/>
+        <source>OK</source>
+        <translation>Kale</translation>
+    </message>
+</context>
+<context>
+    <name>Notifyd</name>
+    <message>
+        <location filename="../notifyd.cpp" line="269"/>
+        <source>Clear All</source>
+        <translation>Byerulewo byonna</translation>
+    </message>
+    <message>
+        <location filename="../notifyd.cpp" line="277"/>
+        <source>Options</source>
+        <translation>Ebisoboka</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../notifyd.cpp" line="284"/>
+        <location filename="../notifyd.cpp" line="341"/>
+        <location filename="../notifyd.cpp" line="361"/>
+        <source>%n Unattended Notification(s)</source>
+        <translation>
+            <numerusform>Obubaka obutannakeberwa bwa murundi %n</numerusform>
+            <numerusform>Obubaka obutannakeberwa bwa mirundi %n</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
