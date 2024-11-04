5695b8cc0386d4e65c30b81c37a290623aa9b1f7
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2024-11-04 17:37:59 +0100

    Weblate commits (#404)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd-menu-entry/ru/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-notificationd/ru/
    Translation: LXQt Configuration/lxqt-config-notificationd Menu Entry
    Translation: LXQt Configuration/lxqt-notificationd
    
    Co-authored-by: Golubev Alexander <fatzer2@gmail.com>

--- src/translations/lxqt-notificationd_ru.ts
+++ src/translations/lxqt-notificationd_ru.ts
@@ -24,7 +24,7 @@
     <message>
         <location filename="../notifyd.cpp" line="285"/>
         <source>Do Not Disturb</source>
-        <translation type="unfinished"></translation>
+        <translation>Не беспокоить</translation>
     </message>
     <message>
         <location filename="../notifyd.cpp" line="293"/>
