8174772ee7b6ab223d247cb92e4fd54044c52381
Weblate <noreply@weblate.org>
2025-02-05 11:25:30 +0100

    Weblate commits
    Co-authored-by: EggSupernova <leomelo34@protonmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd-menu-entry/pt_BR/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-notificationd/pt_BR/
    Translation: LXQt Configuration/lxqt-config-notificationd Menu Entry
    Translation: LXQt Configuration/lxqt-notificationd

--- src/translations/lxqt-notificationd_pt_BR.ts
+++ src/translations/lxqt-notificationd_pt_BR.ts
@@ -24,7 +24,7 @@
     <message>
         <location filename="../notifyd.cpp" line="285"/>
         <source>Do Not Disturb</source>
-        <translation type="unfinished"></translation>
+        <translation>Não Perturbe</translation>
     </message>
     <message>
         <location filename="../notifyd.cpp" line="293"/>
