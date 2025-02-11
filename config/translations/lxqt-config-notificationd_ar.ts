918601a38843543cadf81c1dec7b4061401cefb8
Weblate <noreply@weblate.org>
2025-02-11 16:57:16 +0100

    Weblate commits
    Co-authored-by: Ali Alhaidary علي الحيدري <ali.alhaidary@the5stars.org>
    Co-authored-by: Jan Rolski <wbcwknvstb@proton.me>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/ar/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/pl/
    Translation: LXQt Configuration/lxqt-config-notificationd

--- config/translations/lxqt-config-notificationd_ar.ts
+++ config/translations/lxqt-config-notificationd_ar.ts
@@ -7,17 +7,17 @@
         <location filename="../advancedsettings.ui" line="39"/>
         <location filename="../advancedsettings.ui" line="49"/>
         <source>Use the top of the notification as shown.</source>
-        <translation type="unfinished"></translation>
+        <translation>استخدم الجزء العلوي من الإشعار كما هو موضح.</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="42"/>
         <source>Ignore these notifications:</source>
-        <translation type="unfinished"></translation>
+        <translation>تجاهل هذه الإشعارات:</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="52"/>
         <source>blueman,Screen Grab</source>
-        <translation type="unfinished"></translation>
+        <translation>بلو مان، لقطة شاشة</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="67"/>
