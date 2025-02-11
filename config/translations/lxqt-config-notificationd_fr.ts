eb0e4ce639fc86e8df0193b0999f9ba50fbc5112
Standreas <standreas@riseup.net>
2025-02-11 17:18:12 +0100

    Updated *.ts files (#425)

--- config/translations/lxqt-config-notificationd_fr.ts
+++ config/translations/lxqt-config-notificationd_fr.ts
@@ -15,11 +15,6 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../advancedsettings.ui" line="52"/>
-        <source>blueman,Screen Grab</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../advancedsettings.ui" line="67"/>
         <source>Duration</source>
         <translation>Durée</translation>
