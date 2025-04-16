54e591f4f6e43433a50b1260adf392f98ef78495
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-04-16 09:05:15 +0200

    Weblate commits (#429)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/ca/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/tr/
    Translation: LXQt Configuration/lxqt-config-notificationd
    
    Co-authored-by: Davidmp <opensusecatala@gmail.com>
    Co-authored-by: Dr. Ayhan YALÇINSOY <ayhanyalcinsoy@pisilinux.org>

--- config/translations/lxqt-config-notificationd_tr.ts
+++ config/translations/lxqt-config-notificationd_tr.ts
@@ -7,12 +7,12 @@
         <location filename="../advancedsettings.ui" line="39"/>
         <location filename="../advancedsettings.ui" line="49"/>
         <source>Use the top of the notification as shown.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bildirimin üst kısmını gösterildiği gibi kullanın.</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="42"/>
         <source>Ignore these notifications:</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu bildirimleri yoksay:</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="67"/>
