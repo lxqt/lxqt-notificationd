41436751964a66fa0652f313192e51e43759872e
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-06-20 16:16:52 +0200

    Weblate commits (#433)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd-menu-entry/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/el/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/hr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-notificationd/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-notificationd/hr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/notification-daemon-module-name/sv/
    Translation: LXQt Configuration/Notification Daemon Module Name
    Translation: LXQt Configuration/lxqt-config-notificationd
    Translation: LXQt Configuration/lxqt-config-notificationd Menu Entry
    Translation: LXQt Configuration/lxqt-notificationd
    
    Co-authored-by: Dimitrios Glentadakis <dglent@free.fr>
    Co-authored-by: Simon Lindén <simon_l91@hotmail.com>
    Co-authored-by: milotype <mail@milotype.de>
    Co-authored-by: novakreo <s92m@benj.dropbear.id.au>

--- config/translations/lxqt-config-notificationd_sv.ts
+++ config/translations/lxqt-config-notificationd_sv.ts
@@ -1,73 +1,73 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="hr">
+<TS version="2.1" language="sv">
 <context>
     <name>AdvancedSettings</name>
-    <message>
-        <location filename="../advancedsettings.ui" line="67"/>
-        <source>Duration</source>
-        <translation>Trajanje</translation>
-    </message>
-    <message>
-        <location filename="../advancedsettings.ui" line="79"/>
-        <source>Some notifications set their own on-screen duration.</source>
-        <translation>Neke obavijesti postavljaju vlastita trajanja prikaza.</translation>
-    </message>
-    <message>
-        <location filename="../advancedsettings.ui" line="91"/>
-        <source>Default duration:</source>
-        <translation>Standardno trajanje:</translation>
-    </message>
-    <message>
-        <location filename="../advancedsettings.ui" line="98"/>
-        <source> sec</source>
-        <translation> s</translation>
-    </message>
-    <message>
-        <location filename="../advancedsettings.ui" line="17"/>
-        <source>Unattended Notifications</source>
-        <translation>Nepročitane obavijesti</translation>
-    </message>
-    <message>
-        <location filename="../advancedsettings.ui" line="25"/>
-        <source>How many to save:</source>
-        <translation>Koliko obavijesti čuvati:</translation>
-    </message>
     <message>
         <location filename="../advancedsettings.ui" line="39"/>
         <location filename="../advancedsettings.ui" line="49"/>
         <source>Use the top of the notification as shown.</source>
-        <translation>Koristi gornji dio obavijesti kao što je prikazano.</translation>
+        <translation>Använd den övre delen av meddelandet enligt bilden.</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="42"/>
         <source>Ignore these notifications:</source>
-        <translation>Zanemari ove obavijesti:</translation>
+        <translation>Ignorera de här aviseringarna:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="67"/>
+        <source>Duration</source>
+        <translation>Visningstid</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="79"/>
+        <source>Some notifications set their own on-screen duration.</source>
+        <translation>En del aviseringar har inbyggd visningstid.</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="91"/>
+        <source>Default duration:</source>
+        <translation>Standard visningstid:</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="98"/>
+        <source> sec</source>
+        <translation> sek</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="126"/>
         <source>Screen</source>
-        <translation>Ekran</translation>
+        <translation>Skärm</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="132"/>
         <source>When unchecked the notification will always show on primary screen</source>
-        <translation>Kad nije označeno, obavijest će se uvijek prikazati na primarnom ekranu</translation>
+        <translation>Om detta lämnas avmarkerat så visas aviseringar alltid på huvudskärmen</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="135"/>
         <source>Show notifications on screen with the mouse</source>
-        <translation>Prikaži obavijesti na ekranu s mišem</translation>
+        <translation>Visa endast avisering på skärmen med muspekare</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="145"/>
         <source>Do Not Disturb</source>
-        <translation>Ne smetaj</translation>
+        <translation>Stör ej</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="151"/>
         <source>Only save notifications</source>
-        <translation>Spremi samo obavijesti</translation>
+        <translation>Visa inte aviseringar, men spara dem</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="17"/>
+        <source>Unattended Notifications</source>
+        <translation>Olästa aviseringar</translation>
+    </message>
+    <message>
+        <location filename="../advancedsettings.ui" line="25"/>
+        <source>How many to save:</source>
+        <translation>Hur många ska lagras:</translation>
     </message>
 </context>
 <context>
@@ -75,12 +75,12 @@
     <message>
         <location filename="../basicsettings.ui" line="17"/>
         <source>Sizes</source>
-        <translation>Veličine</translation>
+        <translation>Storlek</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="25"/>
         <source>Width:</source>
-        <translation>Širina:</translation>
+        <translation>Bredd:</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="32"/>
@@ -91,90 +91,90 @@
     <message>
         <location filename="../basicsettings.ui" line="49"/>
         <source>Spacing:</source>
-        <translation>Razmak:</translation>
+        <translation>Avstånd:</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="68"/>
         <source>Position on screen</source>
-        <translation>Položaj na ekranu</translation>
+        <translation>Placering på skärmen</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="227"/>
         <source>Preview</source>
-        <translation>Pregled</translation>
+        <translation>Förhandsvisning</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="65"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; notifications daemon is slow to respond.
 Keep trying to connect…</source>
-        <translation>&lt;b&gt;Oprez:&lt;/b&gt; demon obavijesti reagira sporo.
-Nastavi se povezivati …</translation>
+        <translation>&lt;b&gt;Varning:&lt;/b&gt; Påminnelsetjänsten svarar långsamt.
+Försöker återkoppla…</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="74"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
 A fallback will be used.</source>
-        <translation>&lt;b&gt;Upozorenje:&lt;/b&gt; Nijedan demon za obavijesti nije pokrenut.
-Koristit će se rezervni.</translation>
+        <translation>&lt;b&gt;Varning:&lt;/b&gt; Ingen påminnelsetjänst är aktiv.
+Skiftar till en reservlösning.</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="77"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
 These settings won&apos;t have any effect on it!</source>
-        <translation>&lt;b&gt;Upozorenje:&lt;/b&gt; Pokrenut je jedan strani demon za obavijesti (%1).
-Ove postavke na njega ne utječu!</translation>
+        <translation>&lt;b&gt;Varning:&lt;/b&gt; Det körs en tredjeparts påminnelsetjänst, (%1).
+De här inställningarna kommer inte påverka den!</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="142"/>
         <source>at top left</source>
-        <translation>gore lijevo</translation>
+        <translation>överst till vänster</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="144"/>
         <source>at top center</source>
-        <translation>gore centrirano</translation>
+        <translation>i övre mitten</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="146"/>
         <source>at top right</source>
-        <translation>gore desno</translation>
+        <translation>överst till höger</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="148"/>
         <source>at center left</source>
-        <translation>centrirano lijevo</translation>
+        <translation>centrerad till vänster</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="150"/>
         <source>at center right</source>
-        <translation>centrirano desno</translation>
+        <translation>centrerad till höger</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="152"/>
         <source>at bottom left</source>
-        <translation>dolje lijevo</translation>
+        <translation>underst till vänster</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="154"/>
         <source>at bottom center</source>
-        <translation>dolje centrirano</translation>
+        <translation>i undre mitten</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="156"/>
         <source>at bottom right</source>
-        <translation>dolje desno</translation>
+        <translation>underst till höger</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="158"/>
         <source>Notification demo </source>
-        <translation>Prikaz obavijesti </translation>
+        <translation>Aviseringstest </translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="159"/>
         <source>This is a test notification.
  All notifications will now appear here on LXQt.</source>
-        <translation>Ovo je testna obavijest.
-Sve obavijesti će se sada ovdje pojaviti na LXQt-u.</translation>
+        <translation>Det här är ett testmeddelande.
+Alla aviseringar i LXQt kommer nu visas här.</translation>
     </message>
 </context>
 <context>
@@ -182,17 +182,17 @@ Sve obavijesti će se sada ovdje pojaviti na LXQt-u.</translation>
     <message>
         <location filename="../mainwindow.cpp" line="41"/>
         <source>Desktop Notifications</source>
-        <translation>Obavijesti na radnoj površini</translation>
+        <translation>Skrivbordsaviseringar</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="44"/>
         <source>General Settings</source>
-        <translation>Opće postavke</translation>
+        <translation>Allmänna inställningar</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="48"/>
         <source>Appearance</source>
-        <translation>Izgled</translation>
+        <translation>Utseende</translation>
     </message>
 </context>
 </TS>
